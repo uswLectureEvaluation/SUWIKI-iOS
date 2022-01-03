b3efdc94c9f9a1d6a7e503d6fc72a2f867d79826
sozohoy <49385546+sozohoy@users.noreply.github.com>
2022-01-03 22:02:04 +0900

    Delete uswTimeTable1.0 directory

--- uswTimeTable1.0/Pods/Target Support Files/Elliotable/Elliotable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Elliotable : NSObject
-@end
-@implementation PodsDummy_Elliotable
-@end
--- uswTimeTable1.0/Pods/Target Support Files/Pods-uswTimeTable1.0/Pods-uswTimeTable1.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_uswTimeTable1_0 : NSObject
-@end
-@implementation PodsDummy_Pods_uswTimeTable1_0
-@end
