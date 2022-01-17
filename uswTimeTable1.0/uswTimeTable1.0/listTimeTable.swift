6d98de1f53ac00a2f41af98193a1134807b36f5f
sozohoy <sozohoy@gmail.com>
2022-01-17 22:38:27 +0900

    firebase - uswTimeTable 연동 1차

--- uswTimeTable1.0/uswTimeTable1.0/listTimeTable.swift
+++ uswTimeTable1.0/uswTimeTable1.0/listTimeTable.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 class listTimeTable: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
