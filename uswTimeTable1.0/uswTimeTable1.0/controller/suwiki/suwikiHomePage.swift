16b9b591abaeb04ddc1fb7f1a0982cf792076c64
sozohoy <sozohoy@gmail.com>
2022-04-03 15:58:12 +0900

    [ADD] mainPageTableView 추가 [IMPROVE] cell에 data 출력 완료

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/suwikiHomePage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/suwikiHomePage.swift
@@ -9,10 +9,13 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class suwikiHomePage: UIViewController { // main Page == tableView 구현 스크롤 최대 10개 제한
+class suwikiHomePage: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // main Page == tableView 구현 스크롤 최대 10개 제한
+
+    @IBOutlet weak var tableView: UITableView!
     var viewData: Array<homePageData> = []
-    
+    var viewData2: Array<homePageData> = []
     override func viewDidLoad() {
         navigationBarHidden()
         super.viewDidLoad()
@@ -22,13 +25,45 @@ class suwikiHomePage: UIViewController { // main Page == tableView 구현 스크
     
     @IBAction func testApiBtn(_ sender: Any) {
         getMainPage()
+        viewData2 = viewData
+        print(viewData)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewData2.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let mainCell = tableView.dequeueReusableCell(withIdentifier: "mainCell", for: indexPath) as! mainPageCell
+        mainCell.lectureName.text = viewData[indexPath.row].lectureName
+        mainCell.lectureType.text = viewData[indexPath.row].lectureType
+        mainCell.lectureTotalAvg.text = "\(viewData[indexPath.row].lectureTotalAvg)"
+        mainCell.professor.text = viewData[indexPath.row].professor
+        /*
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! customCell
+        cell.courseTxtField.text = filteredUswCourse[indexPath.row].courseName
+        cell.roomTxtField.text = filteredUswCourse[indexPath.row].roomName
+        cell.professorTxtField.text = filteredUswCourse[indexPath.row].professor
+        cell.numTxtField.text = "\(filteredUswCourse[indexPath.row].num)학년,"
+        cell.classTxtField.text = "\(filteredUswCourse[indexPath.row].classification),"
+        cell.majorTxtField.text = filteredUswCourse[indexPath.row].major
+
+        let bgColorView = UIView()
+        bgColorView.backgroundColor = UIColor.white
+        cell.selectedBackgroundView = bgColorView
+        return cell
+         */
+        return mainCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 115.0
     }
     
     func getMainPage(){
         let url = "https://api.suwiki.kr/lecture/findAllList"
-        
-        
-        
+    
         
         AF.request(url, method: .get, encoding: JSONEncoding.default).responseJSON { (response) in
             let data = response.data
@@ -41,8 +76,10 @@ class suwikiHomePage: UIViewController { // main Page == tableView 구현 스크
                 self.viewData.append(readData)
                             
             }
+            self.viewData2 = self.viewData
             print(self.viewData)
-           
+            self.tableView?.reloadData()
+
         }
         
     }
@@ -52,20 +89,16 @@ class suwikiHomePage: UIViewController { // main Page == tableView 구현 스크
             self.navigationController?.navigationBar.isHidden = true
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
 class mainPageCell: UITableViewCell {
 
+    @IBOutlet weak var lectureName: UILabel!
+    @IBOutlet weak var lectureType: UILabel!
+    @IBOutlet weak var lectureTotalAvg: UILabel!
+    @IBOutlet weak var professor: UILabel!
     
     override func layoutSubviews() {
         super.layoutSubviews()
