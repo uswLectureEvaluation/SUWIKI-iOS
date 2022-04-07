3651c74163edb2b941ae1518901f3c0875e7de93
sozohoy <sozohoy@gmail.com>
2022-04-07 19:16:16 +0900

    [IMPROVE] 자세히보기 lecture 부분 구현 완료

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/lectureDetailedInformationPage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/lectureDetailedInformationPage.swift
@@ -16,28 +16,41 @@ class lectureDetailedInformationPage: UIViewController {
     @IBOutlet weak var lectureName: UILabel!
     @IBOutlet weak var professor: UILabel!
     
-    
+    @IBOutlet weak var lectureHoneyAvg: UILabel!
+    @IBOutlet weak var lectureLearningAvg: UILabel!
+    @IBOutlet weak var lectureSatisAvg: UILabel!
+  
     var detailLectureArray: Array<detailLecture> = []
-    let keychain = KeychainSwift()
     var lectureId = 0
+    let keychain = KeychainSwift()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        getDetailPage()
-
         lectureView.layer.borderWidth = 1.0
         lectureView.layer.borderColor = UIColor.lightGray.cgColor
         lectureView.layer.cornerRadius = 12.0
+        super.viewDidLoad()
+        getDetailPage()
         
 
+    }
+    
+    func lectureViewUpdate(){
+        lectureName.text = detailLectureArray[0].lectureName
         lectureName.sizeToFit()
+        professor.text = detailLectureArray[0].professor
+        professor.sizeToFit()
+        lectureHoneyAvg.text = detailLectureArray[0].lectureHoneyAvg
+        lectureLearningAvg.text = detailLectureArray[0].lectureLearningAvg
+        lectureSatisAvg.text = detailLectureArray[0].lectureSatisfactionAvg
+        print("lectureName")
+        print(lectureName.text)
     }
     
     func getDetailPage(){
         let url = "https://api.suwiki.kr/lecture/?lectureId=\(lectureId)"
         
         let headers: HTTPHeaders = [
-            "AccessToken" : String(keychain.get("AccessToken") ?? "")
+            "Authorization" : String(keychain.get("AccessToken") ?? "")
         ]
 
             
@@ -46,16 +59,16 @@ class lectureDetailedInformationPage: UIViewController {
             let data = response.value
             
             let json = JSON(data!)["data"]
-            
+            print(json)
             let totalAvg = String(format: "%.1f", round(json["lectureTotalAvg"].floatValue * 1000) / 1000)
             let totalSatisfactionAvg = String(format: "%.1f", round(json["lectureSatisfactionAvg"].floatValue * 1000) / 1000)
             let totalHoneyAvg = String(format: "%.1f", round(json["lectureHoneyAvg"].floatValue * 1000) / 1000)
             let totalLearningAvg = String(format: "%.1f", round(json["lectureLearningAvg"].floatValue * 1000) / 1000)
             
             let detailLectureData = detailLecture(id: json["id"].intValue, semester: json["semester"].stringValue, professor: json["professor"].stringValue, lectureType: json["lectureType"].stringValue, lectureName: json["lectureName"].stringValue, lectureTotalAvg: totalAvg, lectureSatisfactionAvg: totalSatisfactionAvg, lectureHoneyAvg: totalHoneyAvg, lectureLearningAvg: totalLearningAvg, lectureTeamAvg: json["lectureTeamAvg"].floatValue, lectureDifficultyAvg: json["lectureDifficultyAvg"].floatValue, lectureHomeworkAvg: json["lectureHomeworkAvg"].floatValue)
-            
+            print(detailLectureData)
             self.detailLectureArray.append(detailLectureData)
-            
+            self.lectureViewUpdate()
             
         }
         
