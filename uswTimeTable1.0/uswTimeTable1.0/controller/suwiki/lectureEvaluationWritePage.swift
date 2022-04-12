562c3ea90cdba85d3c605b222094773f08402a99
sozohoy <sozohoy@gmail.com>
2022-04-12 21:29:07 +0900

    [IMPROVE] 조모임, 과제, 학점 기능 구현 완료

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/lectureEvaluationWritePage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/lectureEvaluationWritePage.swift
@@ -75,37 +75,46 @@ class lectureEvaluationWritePage: UIViewController {
     @IBAction func noTeamBtnClicked(_ sender: Any) {
         teamWorkTypeCheck(teamWork: "no")
         teamWorkType.checkPoint(no: teamWorkType.noTeamWork, have: teamWorkType.haveTeamWork)
+        teamWorkPointCheck()
     }
     @IBAction func haveTeamBtnClicked(_ sender: Any) {
         teamWorkTypeCheck(teamWork: "have")
         teamWorkType.checkPoint(no: teamWorkType.noTeamWork, have: teamWorkType.haveTeamWork)
+        teamWorkPointCheck()
     }
     
     @IBAction func noHomeworkBtnClicked(_ sender: Any) {
         homeworkTypeCheck(homework: "no")
         homeworkType.checkPoint(no: homeworkType.noHomework, usually: homeworkType.usuallyHomework, many: homeworkType.manyHomework)
+        homeworkPointCheck()
     }
     @IBAction func usuallyHomeworkBtnClicked(_ sender: Any) {
         homeworkTypeCheck(homework: "usually")
         homeworkType.checkPoint(no: homeworkType.noHomework, usually: homeworkType.usuallyHomework, many: homeworkType.manyHomework)
+        homeworkPointCheck()
     }
     @IBAction func manyHomeworkBtnClicked(_ sender: Any) {
         homeworkTypeCheck(homework: "many")
         homeworkType.checkPoint(no: homeworkType.noHomework, usually: homeworkType.usuallyHomework, many: homeworkType.manyHomework)
+        print(homeworkType.homeworkPoint)
+        homeworkPointCheck()
     }
     
     
     @IBAction func easyDifficultyBtnClicked(_ sender: Any) {
         difficultyTypeCheck(difficulty: "easy")
         difficultyType.checkPoint(easy: difficultyType.easyDifficulty, normal: difficultyType.normalDifficulty, hard: difficultyType.hardDifficulty)
+        difficultyPointCheck()
     }
     @IBAction func normalDifficultyBtnClicked(_ sender: Any) {
         difficultyTypeCheck(difficulty: "normal")
         difficultyType.checkPoint(easy: difficultyType.easyDifficulty, normal: difficultyType.normalDifficulty, hard: difficultyType.hardDifficulty)
+        difficultyPointCheck()
     }
     @IBAction func hardDifficultyBtnClicked(_ sender: Any) {
         difficultyTypeCheck(difficulty: "hard")
         difficultyType.checkPoint(easy: difficultyType.easyDifficulty, normal: difficultyType.normalDifficulty, hard: difficultyType.hardDifficulty)
+        difficultyPointCheck()
     }
     
     
@@ -155,11 +164,11 @@ class lectureEvaluationWritePage: UIViewController {
             
         } else if homework == "many"{
             if homeworkType.manyHomework == true {
-                homeworkType.manyHomework = true
+                homeworkType.manyHomework = false
                 homeworkType.noHomework = false
                 homeworkType.usuallyHomework = false
             } else {
-                homeworkType.manyHomework = false
+                homeworkType.manyHomework = true
                 homeworkType.noHomework = false
                 homeworkType.usuallyHomework = false
             }
@@ -203,5 +212,60 @@ class lectureEvaluationWritePage: UIViewController {
         }
     }
     
+    func teamWorkPointCheck(){
+        if teamWorkType.teamWorkPoint == 0 {
+            teamNoBtn.setTitleColor(.red, for: .normal)
+            teamHaveBtn.setTitleColor(.darkGray, for: .normal)
+        } else if teamWorkType.teamWorkPoint == 1 {
+            teamNoBtn.setTitleColor(.darkGray, for: .normal)
+            teamHaveBtn.setTitleColor(.red, for: .normal)
+        } else {
+            teamNoBtn.setTitleColor(.darkGray, for: .normal)
+            teamHaveBtn.setTitleColor(.darkGray, for: .normal)
+        }
+    }
+    
+    func homeworkPointCheck(){
+        if homeworkType.homeworkPoint == 0{
+            homeworkNoBtn.setTitleColor(.red, for: .normal)
+            homeworkUsuallyBtn.setTitleColor(.darkGray, for: .normal)
+            homeworkManyBtn.setTitleColor(.darkGray, for: .normal)
+        } else if homeworkType.homeworkPoint == 1{
+            homeworkNoBtn.setTitleColor(.darkGray, for: .normal)
+            homeworkUsuallyBtn.setTitleColor(.red, for: .normal)
+            homeworkManyBtn.setTitleColor(.darkGray, for: .normal)
+        } else if homeworkType.homeworkPoint == 2 {
+            homeworkNoBtn.setTitleColor(.darkGray, for: .normal)
+            homeworkUsuallyBtn.setTitleColor(.darkGray, for: .normal)
+            homeworkManyBtn.setTitleColor(.red, for: .normal)
+        } else {
+            homeworkNoBtn.setTitleColor(.darkGray, for: .normal)
+            homeworkUsuallyBtn.setTitleColor(.darkGray, for: .normal)
+            homeworkManyBtn.setTitleColor(.darkGray, for: .normal)
+        }
+    }
+    
+    func difficultyPointCheck(){
+        if difficultyType.difficultyPoint == 0{
+            easyDifficultyBtn.setTitleColor(.red, for: .normal)
+            normalDifficultyBtn.setTitleColor(.darkGray, for: .normal)
+            hardDiffcultyBtn.setTitleColor(.darkGray, for: .normal)
+        } else if difficultyType.difficultyPoint == 1{
+            easyDifficultyBtn.setTitleColor(.darkGray, for: .normal)
+            normalDifficultyBtn.setTitleColor(.red, for: .normal)
+            hardDiffcultyBtn.setTitleColor(.darkGray, for: .normal)
+        } else if difficultyType.difficultyPoint == 2{
+            easyDifficultyBtn.setTitleColor(.darkGray, for: .normal)
+            normalDifficultyBtn.setTitleColor(.darkGray, for: .normal)
+            hardDiffcultyBtn.setTitleColor(.red, for: .normal)
+        } else {
+            easyDifficultyBtn.setTitleColor(.darkGray, for: .normal)
+            normalDifficultyBtn.setTitleColor(.darkGray, for: .normal)
+            hardDiffcultyBtn.setTitleColor(.darkGray, for: .normal)
+        }
+    }
+    
         
 }
+
+
