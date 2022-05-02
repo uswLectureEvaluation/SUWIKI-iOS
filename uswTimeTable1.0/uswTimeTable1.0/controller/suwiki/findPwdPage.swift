1a5daa54349468b6a45326ffcf1cf9b41724099d
sozohoy <sozohoy@gmail.com>
2022-05-02 19:06:49 +0900

    [ADD] 비밀번호 찾기 추가

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/findPwdPage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/findPwdPage.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class findPwdPage: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +20,8 @@ class findPwdPage: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendBtnClicked(_ sender: Any) {
     }
-    */
-
+    
 }
