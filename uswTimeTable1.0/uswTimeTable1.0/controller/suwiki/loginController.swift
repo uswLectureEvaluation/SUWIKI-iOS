561e18fa1626bab24bd77ea184ffb06c8cfeabc1
sozohoy <sozohoy@gmail.com>
2022-05-21 22:41:33 +0900

    [IMPROVE] 로그인 디자인 수정

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/loginController.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/loginController.swift
@@ -16,9 +16,18 @@ import SwiftUI
 class loginController: UIViewController {
 
     
+    @IBOutlet weak var idTextFieldLine: UIView!
+    @IBOutlet var passwordTextFieldLine: UIView!
+    
     let keychain = KeychainSwift()
     let loginModel = userModel()
     var useAutoLogin = 0
+
+    let bottomLine1 = CALayer()
+    let bottomLine2 = CALayer()
+
+    let colorLiteralBlue = #colorLiteral(red: 0.2016981244, green: 0.4248289466, blue: 0.9915582538, alpha: 1)
+    let colorLiteralPurple = #colorLiteral(red: 0.4726856351, green: 0, blue: 0.9996752143, alpha: 1)
 
     
     @IBOutlet weak var idTextField: UITextField!
@@ -35,7 +44,11 @@ class loginController: UIViewController {
             useAutoLogin = 1
             loginCheck(id: UserDefaults.standard.string(forKey: "id")!, pwd: UserDefaults.standard.string(forKey: "pwd")!)
         }
-             
+        
+        idTextField.addTarget(self, action: #selector(idTextFieldClicked), for: .touchDown)
+        passwordTextField.addTarget(self, action: #selector(passwordTextFieldClicked), for: .touchDown)
+        
+        //cell.adjustBtn.addTarget(self, action: #selector(adjustEvaluationBtnClicked), for: .touchUpInside)
         super.viewDidLoad()
         
 
@@ -43,21 +56,19 @@ class loginController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-        
-        let bottomLine1 = CALayer()
+        /*
         bottomLine1.frame = CGRect(x: 0, y: idTextField.frame.size.height + 16, width: idTextField.frame.width, height: 1)
         bottomLine1.borderColor = UIColor.systemGray2.cgColor
         bottomLine1.borderWidth = 1.0
         idTextField.borderStyle = .none
         idTextField.layer.addSublayer(bottomLine1)
         
-        let bottomLine2 = CALayer()
         bottomLine2.frame = CGRect(x: 0, y: passwordTextField.frame.size.height + 16, width: passwordTextField.frame.width, height: 1)
         bottomLine2.borderColor = UIColor.systemGray2.cgColor
         bottomLine2.borderWidth = 1.0
         passwordTextField.borderStyle = .none
         passwordTextField.layer.addSublayer(bottomLine2)
-        
+        */
     }
     
     
@@ -72,24 +83,30 @@ class loginController: UIViewController {
         if loginModel.isValidId(id: id){
             if let removeable = self.view.viewWithTag(100){
                 removeable.removeFromSuperview()
+                idTextFieldLine.layer.backgroundColor = UIColor.lightGray.cgColor
             }
         } else {
             emailLabel.isHidden = false
             emailLabel.text = "아이디 형식을 확인해 주세요"
             emailLabel.textColor = UIColor.red
             emailLabel.tag = 100
+            idTextFieldLine.layer.backgroundColor = colorLiteralPurple.cgColor
+            
             self.view.addSubview(emailLabel)
         } // 아이디 형식 오류
             
         if loginModel.isValidPassword(pwd: pwd){
             if let removeable = self.view.viewWithTag(101){
                 removeable.removeFromSuperview()
+                passwordTextFieldLine.layer.backgroundColor = UIColor.lightGray.cgColor
             }
         } else {
             passwordLabel.isHidden = false
             passwordLabel.text = "비밀번호 형식을 확인해 주세요"
             passwordLabel.textColor = UIColor.red
             passwordLabel.tag = 101
+            passwordTextFieldLine.layer.backgroundColor = colorLiteralPurple.cgColor
+            
             self.view.addSubview(passwordLabel)
         } // 비밀번호 형식 오류
             
@@ -157,4 +174,15 @@ class loginController: UIViewController {
             passwordTextField.becomeFirstResponder()
         }
     }
+    
+    @objc func idTextFieldClicked(_ sender: UITextField){
+        idTextFieldLine.layer.backgroundColor = colorLiteralBlue.cgColor
+        passwordTextFieldLine.layer.backgroundColor = UIColor.lightGray.cgColor
+    }
+    
+    @objc func passwordTextFieldClicked(_ sender: UITextField) {
+        idTextFieldLine.layer.backgroundColor = UIColor.lightGray.cgColor
+        passwordTextFieldLine.layer.backgroundColor = colorLiteralBlue.cgColor
+    }
+    
 }
