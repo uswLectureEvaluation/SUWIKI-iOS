67ae75fa6ff64ecfcb465f73cd9b7a6683b11b06
sozohoy <sozohoy@gmail.com>
2022-05-23 19:48:24 +0900

    [IMPROVE] 회원가입 뷰 UI 일부 추가

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/signUpPage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/signUpPage.swift
@@ -25,6 +25,8 @@ class signUpPage: UIViewController {
     let colorLiteralBlue = #colorLiteral(red: 0.2016981244, green: 0.4248289466, blue: 0.9915582538, alpha: 1)
     let colorLiteralPurple = #colorLiteral(red: 0.4726856351, green: 0, blue: 0.9996752143, alpha: 1)
 
+    var addPasswordLabel = false
+    var addPasswordCheckLabel = false
     
     let loginModel = userModel()
     
@@ -80,17 +82,59 @@ class signUpPage: UIViewController {
     
     
     @objc func passwordTextTypeCheck(_ sender: UITextField){
-        if passwordTextField.text?.count ?? 0 < 8 {
+        let passwordLabel = UILabel(frame: CGRect(x: 16, y: passwordTextFieldBottomLine.frame.maxY + 2, width: 120, height: 18))
+
+        if passwordTextField.text?.count ?? 0 < 8 { // if문 내부에 정규식 true 반환할 경우 추가 필요
+            
             passwordTextFieldBottomLine.layer.backgroundColor = colorLiteralPurple.cgColor
+            
+            if addPasswordLabel == false {
+                print("addpasswdLabel")
+                addPasswordLabel = true
+                passwordLabel.text = "8자 이상 입력하세요."
+                passwordLabel.textColor = colorLiteralPurple
+                passwordLabel.font = passwordLabel.font.withSize(12)
+                passwordLabel.tag = 100
+                self.view.addSubview(passwordLabel)
+            
+            }
+            
         } else {
+            if let removeable = self.view.viewWithTag(100) {
+                removeable.removeFromSuperview()
+                addPasswordLabel = false
+            }
             passwordTextFieldBottomLine.layer.backgroundColor = colorLiteralBlue.cgColor
         }
     }
     
     @objc func passwordCheckTextTypeCheck(_ sender: UITextField){
+        let passwordCheckLabel = UILabel(frame: CGRect(x: 16, y: passwordCheckTextFieldBottomLine.frame.maxY + 2, width: 190, height: 18))
+        
+        
         if passwordTextField.text == passwordCheckTextField.text {
             passwordCheckTextFieldBottomLine.layer.backgroundColor = colorLiteralBlue.cgColor
+            
+            if let removeable = self.view.viewWithTag(101) {
+                
+                removeable.removeFromSuperview()
+                addPasswordCheckLabel = false
+                
+            }
+
         } else {
+            
+            if addPasswordCheckLabel == false {
+                
+                addPasswordCheckLabel = true
+                passwordCheckLabel.text = "비밀번호가 일치하지 않습니다."
+                passwordCheckLabel.textColor = colorLiteralPurple
+                passwordCheckLabel.font = passwordCheckLabel.font.withSize(12)
+                passwordCheckLabel.tag = 101
+                self.view.addSubview(passwordCheckLabel)
+                
+            }
+
             passwordCheckTextFieldBottomLine.layer.backgroundColor = colorLiteralPurple.cgColor
         }
       
