b6bee65672d26fa37ab813f374032d15fe9c6f80
sozohoy <sozohoy@gmail.com>
2022-06-16 21:53:28 +0900

    [IMPROVE] 강의평가 수정 2차 및 키보드 터치 시 view 좌표 수정기능 완료

--- uswTimeTable1.0/uswTimeTable1.0/AppDelegate.swift
+++ uswTimeTable1.0/uswTimeTable1.0/AppDelegate.swift
@@ -125,4 +125,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension UIViewController {
+    func hideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+            action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
 
