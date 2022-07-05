ce9df696413834678094d243f63cde0f8f62dc25
sozohoy <sozohoy@gmail.com>
2022-07-05 19:52:31 +0900

    modify - 데이터 저장 방식 변경

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/MajorCategoryPage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/MajorCategoryPage.swift
@@ -11,18 +11,51 @@ import Alamofire
 import SwiftyJSON
 import KeychainSwift
 
-class MajorCategoryPage: UIViewController {
+class MajorCategoryPage: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    // 버튼 눌렀을 때 api 호출 --> majorType 넘겨준다.
+    
+    @IBOutlet weak var tableView: UITableView!
     
     let keychain = KeychainSwift()
     
+    private var tableViewUpdateData: Array<MajorCategory> = []
+    
+    private var searchTableViewData: Array<MajorCategory> = []
+
+    private var
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getMajorType()
 
+        let majorCellName = UINib(nibName: "MajorCategoryCell", bundle: nil)
+        tableView.register(majorCellName, forCellReuseIdentifier: "majorCell")
+        
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 63
+        self.tableView.reloadData()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewUpdateData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "majorCell", for: indexPath) as! MajorCategoryCell
+        let data = tableViewUpdateData[indexPath.row]
+        
+        cell.majorTypeLabel.text = "\(data.stringValue)"
+        return cell
+    }
+    
     func getMajorType(){
-        let url = "https://api.suwiki.kr/lecture/search/"
+        let url = "https://api.suwiki.kr/suwiki/majorType"
         
         let parameter: Parameters = [
             "Authorization" : String(keychain.get("AccessToken") ?? "")
@@ -30,20 +63,15 @@ class MajorCategoryPage: UIViewController {
         // JSONEncoding --> URLEncoding으로 변경해야 데이터 넘어옴(파라미터 사용 시)
         AF.request(url, method: .get, parameters: parameter, encoding: URLEncoding.default).responseJSON { (response) in
             let data = response.data
+            
             let json = JSON(data ?? "")
-            print(json)
+            
+            self.tableViewUpdateData = json["data"].arrayValue
+            self.tableView.reloadData()
+            print(self.tableViewUpdateData)
         }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
--- uswTimeTable1.0/uswTimeTable1.0/model/MajorCategoryModel.swift
+++ uswTimeTable1.0/uswTimeTable1.0/model/MajorCategoryModel.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+
+struct MajorCategory{
+    let majorType: String
+}
