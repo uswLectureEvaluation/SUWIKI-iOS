e87767af5fcd5cc816cb95d9c5c202ce4efdf3b0
sozohoy <sozohoy@gmail.com>
2022-07-29 22:09:45 +0900

    add: 회원 탈퇴 page

---
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/QuitAccountPage.swift
@@ -0,0 +1,29 @@
+//
+//  QuitAccountPage.swift
+//  uswTimeTable1.0
+//
+//  Created by 한지석 on 2022/07/29.
+//
+
+import UIKit
+
+class QuitAccountPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
