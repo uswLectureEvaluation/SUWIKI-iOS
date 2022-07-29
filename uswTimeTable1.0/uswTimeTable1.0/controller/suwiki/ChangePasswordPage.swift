e6cf652d7179ec639189ab8414d9cb1aaedc31ef
sozohoy <sozohoy@gmail.com>
2022-07-29 20:53:09 +0900

    feat: 새 비밀번호

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/ChangePasswordPage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/ChangePasswordPage.swift
@@ -23,7 +23,9 @@ class ChangePasswordPage: UIViewController {
     
     var addTextFieldLengthCheck: Bool = false
     var addTextFieldTypeCheck: Bool = false
-
+    
+    var equalPassword: Bool = false
+    var equalPasswordView: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,18 +35,31 @@ class ChangePasswordPage: UIViewController {
         
         newPasswordTextField.isSecureTextEntry = true
         newPasswordTextField.textContentType = .none
-        
+
+        presentPasswordTextField.addTarget(self,
+                                           action: #selector(passwordTextTypeCheck),
+                                           for: .touchDown)
+        presentPasswordTextField.addTarget(self,
+                                           action: #selector(passwordTextTypeCheck),
+                                           for: .editingChanged)
         // Do any additional setup after loading the view.
     }
     
 
     
+    //MARK: - objc passwordChange
     
-    @objc func passwordTextTypeCheck(_ sender: UITextField){
+    @objc func passwordTextTypeCheck(_ sender: UITextField) {
         guard let pwd = presentPasswordTextField.text, !pwd.isEmpty else { return }
         
-        let presentLabel = UILabel(frame: CGRect(x: 16, y: presentBottomLine.frame.maxY + 2, width: 120, height: 18))
-        let presentTypeLabel = UILabel(frame: CGRect(x: 16, y: presentBottomLine.frame.maxY + 2, width: 200, height: 18)) // 정규식 레이블
+        let presentLabel = UILabel(frame: CGRect(x: 20,
+                                                 y: presentBottomLine.frame.maxY + 2,
+                                                 width: 120,
+                                                 height: 18))
+        let presentTypeLabel = UILabel(frame: CGRect(x: 20,
+                                                     y: presentBottomLine.frame.maxY + 2,
+                                                     width: 200,
+                                                     height: 18)) // 정규식 레이블
         
         if presentPasswordTextField.text?.count ?? 0 < 8 { // 비밀번호 8자 이하 입력 시
             
@@ -72,6 +87,7 @@ class ChangePasswordPage: UIViewController {
                 if let removeable = self.view.viewWithTag(100) {
                     removeable.removeFromSuperview()
                 }
+                
                 if let removeable = self.view.viewWithTag(101) {
                     removeable.removeFromSuperview()
                 }
@@ -87,22 +103,49 @@ class ChangePasswordPage: UIViewController {
                 presentBottomLine.layer.backgroundColor = colorLiteralPurple.cgColor
                 
                 if addTextFieldTypeCheck == false {
-                    
                     addTextFieldTypeCheck = true
                     presentTypeLabel.text = "영문, 숫자, 특수문자를 포함하세요."
                     presentTypeLabel.textColor = colorLiteralPurple
                     presentTypeLabel.font = UIFont(name: "Pretendard", size: 12)
                     presentTypeLabel.tag = 101
                     self.view.addSubview(presentTypeLabel)
-                    
+        
                 }
-                
-                
+            }
+        }
+    }
+    
+    @objc func newPasswordTypeCheck(_ sender: UITextField) {
+        guard let pwd = presentPasswordTextField.text, !pwd.isEmpty else { return }
+        
+        let newTypeLabel = UILabel(frame: CGRect(x: 20,
+                                                     y: newBottomLine.frame.maxY + 2,
+                                                     width: 200,
+                                                     height: 18))
+        
+        if newPasswordTextField.text == presentPasswordTextField.text {
+            equalPasswordView = false
+            equalPassword = true
+            newBottomLine.layer.backgroundColor = colorLiteralBlue.cgColor
+            
+            if let removeable = self.view.viewWithTag(102) {
+                removeable.removeFromSuperview()
+            }
+            
+        } else {
+            equalPassword = false
+            newBottomLine.layer.backgroundColor = colorLiteralPurple.cgColor
+            
+            if equalPasswordView == false {
+                equalPasswordView = true
+                newTypeLabel.text = "비밀번호가 일치하지 않습니다."
+                newTypeLabel.textColor = colorLiteralPurple
+                newTypeLabel.font = UIFont(name: "Pretendard", size: 12)
+                newTypeLabel.tag = 102
+                self.view.addSubview(newTypeLabel)
             }
             
         }
-    
-        
     }
     
     
