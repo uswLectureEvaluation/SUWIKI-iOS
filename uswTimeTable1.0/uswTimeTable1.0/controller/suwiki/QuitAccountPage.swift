9c05e5e1c8c6c7aa561d76acdb99afe064179941
sozohoy <sozohoy@gmail.com>
2022-08-10 18:05:11 +0900

    feat: 비밀번호 변경 키보드 수정

--- uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/QuitAccountPage.swift
+++ uswTimeTable1.0/uswTimeTable1.0/controller/suwiki/QuitAccountPage.swift
@@ -217,6 +217,11 @@ class QuitAccountPage: UIViewController {
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+
+          self.view.endEditing(true)
+
+    }
     //MARK: etc.
     
 //    private func showToast(message : String) {
