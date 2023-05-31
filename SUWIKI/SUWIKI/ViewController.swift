97b58df6ee34c878ef1b7504d0b9774254f30d02
sozohoy <sozohoy@gmail.com>
2023-05-31 14:02:34 +0900

    init timetable

---
+++ SUWIKI/SUWIKI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2023/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
