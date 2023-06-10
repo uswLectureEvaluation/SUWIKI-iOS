4a9257ee9c0b31ab24f02299fec05ffa64d2b9f9
sozohoy <sozohoy@gmail.com>
2023-06-10 20:27:58 +0900

    feat: Setting 수정

--- SUWIKI/SUWIKI/SceneDelegate.swift
+++ SUWIKI/SUWIKI/SceneDelegate.swift
@@ -14,11 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
-        let mainViewController = UINavigationController(rootViewController: AddCourseViewController()) // 맨 처음 보여줄 ViewController
-
-        window?.rootViewController = mainViewController
-        window?.makeKeyAndVisible()
+//        window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
+//        let mainViewController = UINavigationController(rootViewController: TimetableViewController()) // 맨 처음 보여줄 ViewController
+//
+//        window?.rootViewController = mainViewController
+//        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
