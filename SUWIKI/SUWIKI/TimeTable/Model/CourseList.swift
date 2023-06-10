b6f63f4425d0f46ca9440cf89e8117afefa03227
sozohoy <sozohoy@gmail.com>
2023-06-10 20:27:42 +0900

    add: TimeTableView, Controller + CourseList

---
+++ SUWIKI/SUWIKI/TimeTable/Model/CourseList.swift
@@ -0,0 +1,8 @@
+//
+//  CourseList.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2023/06/10.
+//
+
+import Foundation
---
+++ SUWIKI/SUWIKI/TimeTable/View/TimetableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimetableViewController.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2023/06/10.
+//
+
+import UIKit
+
+class TimetableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
