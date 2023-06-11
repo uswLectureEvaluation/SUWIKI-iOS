bd6c4d71ead80e2456567c4a0a569bc3a3519587
sozohoy <sozohoy@gmail.com>
2023-06-11 21:28:08 +0900

    feat: 코어데이터 설정

---
+++ SUWIKI/Course+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Course+CoreDataClass.swift
+//  
+//
+//  Created by 한지석 on 2023/06/10.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Course)
+public class Course: NSManagedObject {
+
+}
--- SUWIKI/SUWIKI/TimeTable/Model/CourseList.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CourseList.swift
-//  SUWIKI
-//
-//  Created by 한지석 on 2023/06/10.
-//
-
-import Foundation
