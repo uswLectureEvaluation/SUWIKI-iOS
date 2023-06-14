b46d218470ecfe07535703f305c37438f61e8e3e
sozohoy <sozohoy@gmail.com>
2023-06-15 00:11:04 +0900

    feat: 파이어베이스 데이터 담아놓기 위한 구조체 선언

---
+++ SUWIKI/SUWIKI/InitApp/Model/FirebaseCourse.swift
@@ -0,0 +1,22 @@
+//
+//  FirebaseCourse.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2023/06/14.
+//
+
+import Foundation
+
+struct FirebaseCourse {
+    var classNum: Int
+    var classification: String
+    var courseDay: String
+    var courseName: String
+    var credit: Int
+    var startTime: String
+    var endTime: String
+    var major: String
+    var num: Int
+    var professor: String
+    var roomName: String
+}
