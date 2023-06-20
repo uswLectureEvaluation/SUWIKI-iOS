3c6efb87a64a8535876044fbcc17436e4a3dade8
sozohoy <sozohoy@gmail.com>
2023-06-20 20:49:18 +0900

    feat: CourseCell과 연동된 viewmodel 구현

--- SUWIKI/SUWIKI/Course/AddCourse/View/AddCourseViewModel.swift
+++ SUWIKI/SUWIKI/Course/AddCourse/View/AddCourseViewModel.swift
@@ -8,22 +8,65 @@
 import Foundation
 
 final class AddCourseViewModel {
+    
     let coreDataManager = CoreDataManager.shared
-    var course: [Course] = []
+    var course: FirebaseCourseData
+    
+    init(course: FirebaseCourseData) {
+        self.course = course
+    }
+    
+    //MARK: Output
+    
+    var classification: String {
+        course.classification ?? ""
+    }
+    
+    var courseDay: String {
+        course.courseDay ?? ""
+    }
+    
+    var courseName: String {
+        course.courseName ?? ""
+    }
+    
+    var startTime: String {
+        course.startTime ?? ""
+    }
+    
+    var endTime: String {
+        course.endTime ?? ""
+    }
+    
+    var major: String {
+        "\(String(describing: course.major ?? ""))⎪"
+    }
+    
+    var professor: String {
+        "\(String(describing: course.professor ?? ""))⎪"
+    }
+    
+    var credit: String {
+        "\(course.credit)학점"
+    }
+    
+    var roomName: String {
+        course.roomName ?? ""
+    } 
     
     func getCourse() {
-        course = coreDataManager.getCourseFromCoreData()
-        print("count - \(course.count)")
-        for i in 0..<100 {
-            print(course[i].classification)
-            print(course[i].courseDay)
-            print(course[i].courseName)
-            print(course[i].startTime)
-            print(course[i].endTime)
-            print(course[i].major)
-            print(course[i].roomName)
-            print(course[i].professor)
-        }
+//        course = coreDataManager.getCourseFromCoreData()
+//        print("count - \(course.count)")
+//        for i in 0..<100 {
+//            print(course[i].classification)
+//            print(course[i].courseDay)
+//            print(course[i].courseName)
+//            print(course[i].startTime)
+//            print(course[i].endTime)
+//            print(course[i].major)
+//            print(course[i].roomName)
+//            print(course[i].professor)
+//        }
     }
     
 }
