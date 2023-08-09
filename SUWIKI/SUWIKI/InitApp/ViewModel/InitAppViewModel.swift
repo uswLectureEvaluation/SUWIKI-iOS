47902c7ba87964c0afd048c1752449e56c49cd13
sozohoy <sozohoy@gmail.com>
2023-08-09 15:26:55 +0900

    refactor: 모델 명 수정으로 인한 수정사항 반영

--- SUWIKI/SUWIKI/InitApp/ViewModel/InitAppViewModel.swift
+++ SUWIKI/SUWIKI/InitApp/ViewModel/InitAppViewModel.swift
@@ -15,9 +15,9 @@ class InitAppViewModel {
     let coreDataManager = CoreDataManager.shared
     private let ref = Database.database().reference()
     
-    func fetchFirebaseCourse(completionHandler: @escaping (([FirebaseCourse]?) -> Void)) {
+    func fetchFirebaseCourse(completionHandler: @escaping (([FetchCourse]?) -> ())) {
         ref.getData { error, data in
-            var firebaseCourse: [FirebaseCourse] = []
+            var fetchCourse: [FetchCourse] = []
             guard let snapshot = data else { return }
             for child in snapshot.children {
                 let childSnapshot = child as? DataSnapshot
@@ -25,33 +25,33 @@ class InitAppViewModel {
                     completionHandler(nil)
                     return
                 }
-                let course = FirebaseCourse(classNum: value["classNum"] as? Int ?? 0,
-                                            classification: value["classification"] as? String ?? "",
-                                            courseDay: value["courseDay"] as? String ?? "",
-                                            courseName: value["courseName"] as? String ?? "",
-                                            credit: value["credit"] as? Int ?? 0,
-                                            startTime: value["startTime"] as? String ?? "",
-                                            endTime: value["endTime"] as? String ?? "",
-                                            major: value["major"] as? String ?? "",
-                                            num: value["num"] as? Int ?? 0,
-                                            professor: value["professor"] as? String ?? "",
-                                            roomName: value["roomName"] as? String ?? "")
-                firebaseCourse.append(course)
+                let course = FetchCourse(classNum: value["classNum"] as? Int ?? 0,
+                                         classification: value["classification"] as? String ?? "",
+                                         courseDay: value["courseDay"] as? String ?? "",
+                                         courseName: value["courseName"] as? String ?? "",
+                                         credit: value["credit"] as? Int ?? 0,
+                                         startTime: value["startTime"] as? String ?? "",
+                                         endTime: value["endTime"] as? String ?? "",
+                                         major: value["major"] as? String ?? "",
+                                         num: value["num"] as? Int ?? 0,
+                                         professor: value["professor"] as? String ?? "",
+                                         roomName: value["roomName"] as? String ?? "")
+                fetchCourse.append(course)
             }
-            completionHandler(firebaseCourse)
+            completionHandler(fetchCourse)
         }
     }
     
-    func saveCourseToCoreData(course: [FirebaseCourse]) { // 데이터 저장하기
-        coreDataManager.saveCourseData(course: course)
+    func saveCourseToCoreData(course: [FetchCourse]) { // 데이터 저장하기
+        coreDataManager.saveFirebaseCourse(course: course)
         checkCoreDataCount()
     }
     
     func checkCoreDataCount() { // 저장된 데이터 불러오기 - 시간표 추가 시 필요할듯한데..
-        var temp: [Course] = []
+        var temp: [FirebaseCourse] = []
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         let context = appDelegate.persistentContainer.viewContext
-        if let loadCourse = try? context.fetch(Course.fetchRequest()) {
+        if let loadCourse = try? context.fetch(FirebaseCourse.fetchRequest()) {
             temp = loadCourse
             print(temp.count)
         }
