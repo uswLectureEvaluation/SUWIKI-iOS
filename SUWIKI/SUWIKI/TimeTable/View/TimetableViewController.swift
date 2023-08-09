b9982a6f93301d7f4f16b063fa2012f05251671e
sozohoy <sozohoy@gmail.com>
2023-08-09 15:24:12 +0900

    feat: 강의 추가 모달에서 추가 완료 되었을 경우, combine으로 변화 감지하여 화면 reload

--- SUWIKI/SUWIKI/TimeTable/View/TimetableViewController.swift
+++ SUWIKI/SUWIKI/TimeTable/View/TimetableViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+import Combine
 import Elliotable
 import SnapKit
 import Then
@@ -15,40 +16,61 @@ class TimetableViewController: UIViewController {
     
     @IBOutlet weak var timetable: Elliotable!
     
+//    var addCourseController: AddCourseViewController?
+    var addCourseController = AddCourseViewController()
     let dayString: [String] = ["월", "화", "수", "목", "금"]
-    let courseList: [ElliottEvent] = [ElliottEvent(courseId: "1", courseName: "Hi", roomName: "Hio", professor: "Hio", courseDay: .monday, startTime: "10:00", endTime: "20:00", textColor: .black, backgroundColor: .lightGray)]
-    
+    var viewModel = TimetableViewModel()
+    var viewModel1 = InitAppViewModel()
+    private var cancellables: Set<AnyCancellable> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.getCourse()
         navigationSetUp()
-        addSubView()
-        setUpButtons()
         initTimetable()
+        bind()
     }
+    
+    func bind() {
+        addCourseController.isFinished
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                print("@B 완료됨!")
+                self.viewModel.getCourse()
+                self.updateTimetable()
+            }
+            .store(in: &cancellables)
+    }
+
     
     func navigationSetUp() {
         self.title = "시간표 추가시간표 추가시간표 추가시간표 추가"
         let rightButton = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addButtonTapped))
+        let leftButton = UIBarButtonItem(title: "test", style: .plain, target: self, action: #selector(tempMethod))
         self.navigationItem.rightBarButtonItem = rightButton
+        self.navigationItem.leftBarButtonItem = leftButton
+    }
+    
+    @objc
+    func tempMethod() {
+        CoreDataManager.shared.deleteTimetable()
+//        viewModel.getCourse()
+//        viewModel1.fetchFirebaseCourse { course in
+//            if let course = course {
+//                self.viewModel1.saveCourseToCoreData(course: course)
+//            }
+//
+//        }
+        
+//        viewModel.check1()
+//        viewModel1.checkCoreDataCount()
+//        elliottEvent = viewModel.getCourse()
     }
     
     @objc
     func addButtonTapped() {
-        let addCourseVC = UINavigationController(rootViewController: AddCourseViewController())
+        let addCourseVC = UINavigationController(rootViewController: addCourseController)
         self.present(addCourseVC, animated: true)
-    }
-    
-    func addSubView() {
-//        self.view.addSubview(self.addButton)
-    }
-
-    func setUpButtons() {
-//        self.addButton.snp.makeConstraints {
-//            $0.top.equalToSuperview()
-//            $0.trailing.equalToSuperview()
-//            $0.height.equalTo(44)
-//        }
     }
     
     func initTimetable() {
@@ -80,6 +102,10 @@ class TimetableViewController: UIViewController {
 
 extension TimetableViewController: ElliotableDelegate, ElliotableDataSource {
     
+    func updateTimetable() {
+        timetable.reloadData()
+    }
+    
     func elliotable(elliotable: Elliotable, didSelectCourse selectedCourse: ElliottEvent) {
         print("@Log select")
     }
@@ -89,7 +115,6 @@ extension TimetableViewController: ElliotableDelegate, ElliotableDataSource {
     }
     
     func elliotable(elliotable: Elliotable, at dayPerIndex: Int) -> String {
-        print(dayPerIndex)
         return dayString[dayPerIndex]
     }
     
@@ -98,7 +123,7 @@ extension TimetableViewController: ElliotableDelegate, ElliotableDataSource {
     }
     
     func courseItems(in elliotable: Elliotable) -> [ElliottEvent] {
-        return courseList
+        return viewModel.elliottEvent
     }
 
 }
