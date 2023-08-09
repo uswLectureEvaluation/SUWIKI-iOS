8495892f6624d51aa7a013690e5d5123637f05b5
sozohoy <sozohoy@gmail.com>
2023-08-09 15:27:52 +0900

    feat: CoreData CRUD 분리

--- SUWIKI/SUWIKI/CoreData/CoreDataManager+Delete.swift
+++ SUWIKI/SUWIKI/CoreData/CoreDataManager+Delete.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import CoreData
+
+extension CoreDataManager {
+    
+    func deleteTimetable() {
+//        _ completion: @escaping () -> Void
+        if let context = context {
+//            let delete = context.deletedObjects
+            do {
+                let course = try context.fetch(Course.fetchRequest())
+                for item in course {
+                    context.delete(item)
+                }
+                try context.save()
+                print("@Log delete - \(course)")
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
--- SUWIKI/SUWIKI/CoreData/CoreDataManager+Read.swift
+++ SUWIKI/SUWIKI/CoreData/CoreDataManager+Read.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+import CoreData
+
+extension CoreDataManager {
+    
+}
