d1c1678483597efc5cf15c05afc1f6fba8ddd42b
sozohoy <sozohoy@gmail.com>
2023-08-11 02:25:42 +0900

    feat: 시간표 색상 정리, mainColor

--- SUWIKI/SUWIKI/Extension/UIColor+.swift
+++ SUWIKI/SUWIKI/Extension/UIColor+.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIColor {
+    static let primaryColor = UIColor(red: 52/255, green: 108/255, blue: 253/255, alpha: 1.0)
+    static let timetableColors: [UIColor] = [
+        UIColor(red: 140/255, green: 156/255, blue: 247/255, alpha: 1.0), /* #8c9cf7 */
+        UIColor(red: 103/255, green: 145/255, blue: 253/255, alpha: 1.0), /* #6791fd */
+        UIColor(red: 174/255, green: 102/255, blue: 255/255, alpha: 1.0), /* #ae66ff */
+        UIColor(red: 113/255, green: 71/255, blue: 159/255, alpha: 1.0), /* #71479f */
+        UIColor(red: 134/255, green: 206/255, blue: 193/255, alpha: 1.0), /* #86cec1 */
+        UIColor(red: 40/255, green: 174/255, blue: 150/255, alpha: 1.0), /* #28ae96 */
+        UIColor(red: 245/255, green: 199/255, blue: 144/255, alpha: 1.0), /* #f5c790 */
+        UIColor(red: 245/255, green: 188/255, blue: 80/255, alpha: 1.0), /* #f5bc50 */
+        UIColor(red: 243/255, green: 145/255, blue: 130/255, alpha: 1.0), /* #f39182 */
+        UIColor(red: 205/255, green: 173/255, blue: 141/255, alpha: 1.0), /* #cdad8d */
+        UIColor(red: 176/255, green: 217/255, blue: 139/255, alpha: 1.0) /* #b0d98b */
+    ]
+}
