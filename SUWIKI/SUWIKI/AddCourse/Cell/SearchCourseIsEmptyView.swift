2d410498969af2c93c9223b883942bca27d46e9e
sozohoy <sozohoy@gmail.com>
2023-08-18 02:17:01 +0900

    add: 검색결과 비었을 경우 출력하는 뷰

---
+++ SUWIKI/SUWIKI/AddCourse/Cell/SearchCourseIsEmptyView.swift
@@ -0,0 +1,27 @@
+//
+//  SearchResultIsEmptyView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2023/08/16.
+//
+
+import UIKit
+
+class SearchResultIsEmptyView: UITableViewCell {
+
+    //MARK: UI
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
