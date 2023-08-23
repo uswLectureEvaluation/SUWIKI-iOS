9561ac04472461a71a1e4329fa0f70a3ad427910
sozohoy <sozohoy@gmail.com>
2023-08-23 23:58:58 +0900

    remove: 불필요 코드 제거

--- SUWIKI/SUWIKI/ViewModel/AddCourse/SelectMajorViewModel.swift
+++ SUWIKI/SUWIKI/ViewModel/AddCourse/SelectMajorViewModel.swift
@@ -19,26 +19,12 @@ final class SelectMajorViewModel {
         fetchBookmark()
     }
     
-//    struct Input {
-//        let selectMajor = PublishSubject<String>()
-//    }
-//
-//    struct Output {
-//        let selectedMajor = PublishSubject<String>()
-//    }
-//
-//    let input = Input()
-//    let output = Output()
-//    var cancellable = Set<AnyCancellable>()
-    
     func toggleBookmark(name: String) {
         let index = self.major.firstIndex(where: { $0.name == name })!
         major[index].bookmark.toggle()
         if let bookmarkIndex = bookmark.firstIndex(where: { $0 == name }) {
-            //MARK: 삭제
             bookmark.remove(at: bookmarkIndex)
         } else {
-            //MARK: 추가
             bookmark.append(name)
         }
         updateBookmark()
@@ -51,11 +37,9 @@ final class SelectMajorViewModel {
     func fetchBookmark() {
         if let bookmark = UserDefaults.standard.array(forKey: "bookmark") as? [String] {
             self.bookmark = bookmark
-            print("@Log \(self.bookmark)")
         }
         descendingMajorBookmark()
     }
-    
     
     /// func descendingMajorBookmark: 북마크가 된 학과부터 내림차순으로 정렬합니다.
     func descendingMajorBookmark() {
@@ -72,32 +56,10 @@ final class SelectMajorViewModel {
         }
     }
     
-    func selectedMajor(major: String) {
-        print(major)
-    }
-    
     func pushVC(major: String, currentVC: UIViewController, animated: Bool) {
         let navigationVC = currentVC.navigationController
         let selectCourseVC = SelectCourseViewController(viewModel: SelectCourseListViewModel(major: major))
         navigationVC?.pushViewController(selectCourseVC, animated: animated)
     }
-    
-    //    func handleNextVC(_ index: Int? = nil, fromCurrentVC: UIViewController, animated: Bool) {
-    //        // 기존의 멤버가 있을때
-    //        if let index = index {
-    //            let memberVM = memberViewModelAtIndex(index)
-    //            goToNextVC(with: memberVM, fromCurrentVC: fromCurrentVC, animated: animated)
-    //        // 새로운 멤버 생성시
-    //        } else {
-    //            let newVM = MemberViewModel(dataManager: self.dataManager, with: nil, index: nil)
-    //            goToNextVC(with: newVM, fromCurrentVC: fromCurrentVC, animated: animated)
-    //        }
-    //    }
-    //
-    //    private func goToNextVC(with memberVM: MemberViewModel, fromCurrentVC: UIViewController, animated: Bool) {
-    //        let navVC = fromCurrentVC.navigationController
-    //        let detailVC = DetailViewController(viewModel: memberVM)
-    //        navVC?.pushViewController(detailVC, animated: animated)
-    //    }
     
 }
