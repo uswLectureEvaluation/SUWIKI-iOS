9f30d98de66ec74e7a55685efef258dc9926b689
sozohoy <sozohoy@gmail.com>
2023-08-29 17:54:11 +0900

    feat: setup UI, Constraints, navigationBar

--- SUWIKI/SUWIKI/View/Timetable/AddTimetableViewController.swift
+++ SUWIKI/SUWIKI/View/Timetable/AddTimetableViewController.swift
@@ -7,23 +7,70 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
+
 class AddTimetableViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    //MARK: UI
+    
+    private let timetableNameTextField = UITextField().then {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        $0.leftView = paddingView
+        $0.leftViewMode = .always
+        $0.rightView = paddingView
+        $0.rightViewMode = .always
+        $0.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        $0.layer.cornerRadius = 12
+        $0.backgroundColor = .white
+        $0.placeholder = "시간표 이름을 입력하세요!"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
     }
-    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        self.navigationItem.title = "시간표 추가"
+        self.navigationController?.navigationBar.backgroundColor = .systemGray6
+        self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "xmark.circle.fill"), for: .normal)
+        button.addTarget(self, action: #selector(rightButtonTapped), for: .touchUpInside)
+        button.tintColor = .gray
+            
+        let rightButton = UIBarButtonItem(customView: button)
+        rightButton.customView?.translatesAutoresizingMaskIntoConstraints = false
+        rightButton.customView?.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        rightButton.customView?.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        self.navigationItem.rightBarButtonItem = rightButton
+    }
 
+    private func setupUI() {
+        self.view.backgroundColor = .systemGray6
+        self.view.addSubview(timetableNameTextField)
+    }
+    
+    private func setupConstraints() {
+        self.timetableNameTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(20)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
+            $0.height.equalTo(48)
+        }
+    }
+
+    @objc func rightButtonTapped() {
+        dismiss(animated: true)
+    }
+    
 }
