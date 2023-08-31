6c969a772fa0a0bd2b7c37b5997490b1c60b78f1
sozohoy <sozohoy@gmail.com>
2023-08-31 16:32:57 +0900

    add: timetableListCell

---
+++ SUWIKI/SUWIKI/View/Timetable/Cell/TimetableListCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimetableListCell.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2023/08/30.
+//
+
+import UIKit
+
+class TimetableListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
