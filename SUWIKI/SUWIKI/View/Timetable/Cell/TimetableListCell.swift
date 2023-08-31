d7007e954cacaf70d2ebf41531b1390e9dd192c5
sozohoy <sozohoy@gmail.com>
2023-08-31 16:34:25 +0900

    feat: timetableListCell UI

--- SUWIKI/SUWIKI/View/Timetable/Cell/TimetableListCell.swift
+++ SUWIKI/SUWIKI/View/Timetable/Cell/TimetableListCell.swift
@@ -7,17 +7,70 @@
 
 import UIKit
 
-class TimetableListCell: UITableViewCell {
+import Reusable
+import SnapKit
 
+class TimetableListCell: UITableViewCell, Reusable {
+
+    let semesterLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    }
+    
+    let titleLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        $0.textColor = .darkGray
+    }
+    
+    let removeButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "trash"), for: .normal)
+        $0.tintColor = .red
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func setupUI() {
+        [semesterLabel, titleLabel, removeButton].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        semesterLabel.snp.makeConstraints {
+            $0.top.equalTo(contentView.snp.top).offset(16)
+            $0.leading.equalTo(contentView.snp.leading).offset(16)
+            $0.height.equalTo(22)
+        }
+        removeButton.snp.makeConstraints {
+            $0.centerY.equalTo(contentView.snp.centerY)
+            $0.trailing.equalTo(contentView.snp.trailing).offset(-12)
+            $0.height.equalTo(22)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.bottom.equalTo(contentView.snp.bottom).offset(-12)
+            $0.leading.equalTo(contentView.snp.leading).offset(16)
+            $0.trailing.lessThanOrEqualTo(removeButton.snp.leading).offset(-12)
+            $0.height.equalTo(22)
+        }
     }
 
 }
