0f95fb56e9ea221565b85c5e483641aaa8106535
sozohoy <sozohoy@gmail.com>
2023-09-12 21:07:57 +0900

    add: TimetableEmptyView

---
+++ SUWIKI/SUWIKI/View/Timetable/TimetableEmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  TimetableEmptyView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2023/09/12.
+//
+
+import UIKit
+
+class TimetableEmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
