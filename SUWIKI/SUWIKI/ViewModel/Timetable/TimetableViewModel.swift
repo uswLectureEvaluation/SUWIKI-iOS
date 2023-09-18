9574f1de3c296b349aa296b7b6198b35aa139bba
sozohoy <sozohoy@gmail.com>
2023-09-18 19:22:24 +0900

    feat: default 시간표 기능

--- SUWIKI/SUWIKI/ViewModel/Timetable/TimetableViewModel.swift
+++ SUWIKI/SUWIKI/ViewModel/Timetable/TimetableViewModel.swift
@@ -17,24 +17,34 @@ final class TimetableViewModel {
     private var cancellables: Set<AnyCancellable> = []
     
     @Published var elliottEvent: [ElliottEvent] = []
+    @Published var timetableTitle: String = ""
+    @Published var timetableIsEmpty: Bool = false
     
-    func bind() {
-        
+    init() {
+        fetchTimetable()
+    }
+
+    func fetchTimetable() {
+        let timetable = coreDataManager.fetchTimetableList()
+        self.timetableIsEmpty = timetable.isEmpty
     }
     
-//    func tempIsFinished() {
-//        let addController = AddCourseViewController()
-//        addController.viewModel.$isFinished
-//            .receive(on: DispatchQueue.main)
-//            .sink { isFinished in
-//                print("@Log sink - \(isFinished)")
-//                self.getCourse()
-//            }
-//            .store(in: &addController.cancellable)
-//    }
+    func addTimetable() {
+        coreDataManager.addTimeTable(name: "시간표", semester: calculateSemester())
+        updateTimetable()
+        self.timetableIsEmpty = false
+    }
     
+    func updateTimetable() {
+        guard let id = UserDefaults.standard.value(forKey: "id") as? String,
+              let title = coreDataManager.fetchTimetable(id: id)?.name else {
+            self.timetableTitle = "시간표"
+            return
+        }
+        self.timetableTitle = title
+    }
     
-    func getCourse() {
+    func updateCourse() {
         guard let id = UserDefaults.standard.value(forKey: "id") as? String else {
             return
         }
@@ -58,7 +68,32 @@ final class TimetableViewModel {
               let id = UserDefaults.standard.value(forKey: "id") as? String
         else { return }
         elliottEvent.remove(at: index)
-        coreDataManager.deleteCourse(id: id, courseId: uuid)
+        do {
+            try coreDataManager.deleteCourse(id: id, courseId: uuid)
+        } catch {
+            coreDataManager.handleCoreDataError(error)
+        }
+    }
+    
+    func calculateSemester() -> String {
+        let currentYear = Calendar.current.component(.year, from: Date())
+        let currentMonth = Calendar.current.component(.month, from: Date())
+        if currentMonth <= 6 {
+            return "\(currentYear)년 1학기"
+        } else {
+            return "\(currentYear)년 2학기"
+        }
     }
     
 }
+
+//    func tempIsFinished() {
+//        let addController = AddCourseViewController()
+//        addController.viewModel.$isFinished
+//            .receive(on: DispatchQueue.main)
+//            .sink { isFinished in
+//                print("@Log sink - \(isFinished)")
+//                self.getCourse()
+//            }
+//            .store(in: &addController.cancellable)
+//    }
