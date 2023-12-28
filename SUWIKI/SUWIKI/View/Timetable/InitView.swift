c0306f713a28b6c03935c37069983130d967da4b
sozohoy <sozohoy@gmail.com>
2023-12-28 15:48:34 +0900

    add: InitView

---
+++ SUWIKI/SUWIKI/View/Timetable/InitView.swift
@@ -0,0 +1,20 @@
+//
+//  InitView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 12/28/23.
+//
+
+import UIKit
+
+class InitView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
