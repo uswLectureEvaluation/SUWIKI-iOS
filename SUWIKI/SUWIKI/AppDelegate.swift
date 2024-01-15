8cf71cf5870cc1dbfa7baaef61397253a1d36a55
sozohoy <sozohoy@gmail.com>
2024-01-15 20:51:19 +0900

    remove: 코어데이터 코드 제거(#34)

--- SUWIKI/SUWIKI/AppDelegate.swift
+++ SUWIKI/SUWIKI/AppDelegate.swift
@@ -34,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
-
 //    lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -45,22 +44,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        let container = NSPersistentContainer(name: "SuwikiTimetable")
 //        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
 //            if let error = error as NSError? {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                 
-//                /*
-//                 Typical reasons for an error here include:
-//                 * The parent directory does not exist, cannot be created, or disallows writing.
-//                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-//                 * The device is out of space.
-//                 * The store could not be migrated to the current model version.
-//                 Check the error message to determine what the actual problem was.
-//                 */
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
 //                fatalError("Unresolved error \(error), \(error.userInfo)")
 //            }
 //        })
 //        return container
 //    }()
+
+//    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+//    lazy var persistentContainer: NSPersistentContainer = {
+//        guard let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup) else {
+
+//        }
+//
+
+//        container.loadPersistentStores { storeDescription, error in
+//            if let error = error as NSError? {
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
+//            }
+//        }
+//        return container
+//    }()
+
 
     // MARK: - Core Data Saving support
 
