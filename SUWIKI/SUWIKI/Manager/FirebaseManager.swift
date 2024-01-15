de8690459505e15da2b51dd054e932bcd79bbd35
sozohoy <sozohoy@gmail.com>
2024-01-15 20:48:32 +0900

    bug: 닐 코얼레싱 처리 타입 오류 해결(#34)

--- SUWIKI/SUWIKI/Manager/FirebaseManager.swift
+++ SUWIKI/SUWIKI/Manager/FirebaseManager.swift
@@ -38,20 +38,20 @@ class FirebaseManager {
             return []
         }
         var dictionaries: [[String: Any]] = []
-        var notUpdatedCourseName = "시간표가 업데이트 되지 않았어요."
-        var notUpdatedCourseInfo = "미정"
-        var notUpdatedCourseTime = "22:00"
+        let notUpdatedCourseName = "시간표가 업데이트 되지 않았어요."
+        let notUpdatedCourseInfo = "미정"
+        let notUpdatedCourseTime = "22:00"
         for fetchCourse in value {
             var courseDictionary: [String: Any] = [:]
             courseDictionary["classNum"] = fetchCourse["classNum"] as? String ?? notUpdatedCourseInfo
             courseDictionary["classification"] = fetchCourse["classification"] as? String ?? notUpdatedCourseInfo
             courseDictionary["courseDay"] = fetchCourse["courseDay"] as? String ?? notUpdatedCourseInfo
             courseDictionary["courseName"] = fetchCourse["courseName"] as? String ?? notUpdatedCourseName
-            courseDictionary["credit"] = fetchCourse["credit"] as? Int ?? notUpdatedCourseInfo
+            courseDictionary["credit"] = fetchCourse["credit"] as? Int ?? 0
             courseDictionary["startTime"] = fetchCourse["startTime"] as? String ?? notUpdatedCourseTime
             courseDictionary["endTime"] = fetchCourse["endTime"] as? String ?? notUpdatedCourseTime
             courseDictionary["major"] = fetchCourse["major"] as? String ?? notUpdatedCourseInfo
-            courseDictionary["num"] = fetchCourse["num"] as? Int ?? notUpdatedCourseInfo
+            courseDictionary["num"] = fetchCourse["num"] as? Int ?? 0
             courseDictionary["professor"] = fetchCourse["professor"] as? String ?? notUpdatedCourseInfo
             courseDictionary["roomName"] = fetchCourse["roomName"] as? String ?? notUpdatedCourseInfo
             dictionaries.append(courseDictionary)
