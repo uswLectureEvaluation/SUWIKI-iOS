4c579087d15a3b28d7fc88b45fb2ae67ab33aadf
sozohoy <sozohoy@gmail.com>
2024-01-16 17:17:40 +0900

    feat: Medium 사이즈 위젯 구현(#34)

--- SUWIKI/TimetableWidget/View/TimetableWidgetMedium.swift
+++ SUWIKI/TimetableWidget/View/TimetableWidgetMedium.swift
@@ -8,11 +8,74 @@
 import SwiftUI
 
 struct TimetableWidgetMedium: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-#Preview {
-    TimetableWidgetMedium()
+    @StateObject var viewModel = TimetableWidgetViewModel()
+
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                weekday
+                if viewModel.courses.count > 0 {
+                    VStack {
+                        ForEach(viewModel.courses) { course in
+                            WidgetCell(color: .timetableColors[course.color],
+                                       courseName: course.courseName,
+                                       startTime: course.startTime,
+                                       endTime: course.endTime)
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 2)
+                    .padding(.leading, 12)
+                } else {
+                    dayOff
+                        .padding(.top, 2)
+                        .padding(.leading, 12)
+                }
+
+            }
+            if viewModel.eLearning.count > 0 {
+                Divider()
+                    .padding(.bottom, 4)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("이러닝")
+                            .font(.system(size: 17, weight: .semibold))
+                        Spacer()
+                    }
+                    VStack {
+                        ForEach(viewModel.eLearning) { eLearning in
+                            WidgetCell(color: .timetableColors[eLearning.color],
+                                       courseName: eLearning.courseName)
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 2)
+                    .padding(.leading, 4)
+                }
+            }
+        }
+    }
+
+    var weekday: some View {
+        VStack(alignment: .leading) {
+            Text(viewModel.weekday)
+                .font(.system(size: 20, weight: .semibold))
+            Text(viewModel.date)
+                .font(.system(size: 10, weight: .light))
+            Spacer()
+        }
+    }
+
+    var dayOff: some View {
+        HStack {
+            Rectangle()
+                .frame(width: 4, height: 16)
+                .foregroundStyle(Color.blue)
+            Text("오늘은 강의가 없어요!")
+                .font(.system(size: 12, weight: .semibold))
+            Spacer()
+        }
+    }
+
 }
