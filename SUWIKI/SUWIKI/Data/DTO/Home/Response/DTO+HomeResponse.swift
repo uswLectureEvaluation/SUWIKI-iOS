5df8880fd92c294c4e709a139d0e3c25c46d63fe
sozohoy <sozohoy@gmail.com>
2024-01-24 23:56:37 +0900

    feat: HomeResponse(#38)

--- SUWIKI/SUWIKI/Data/DTO/Home/Response/DTO+HomeResponse.swift
+++ SUWIKI/SUWIKI/Data/DTO/Home/Response/DTO+HomeResponse.swift
@@ -7,3 +7,11 @@
 
 import Foundation
 
+extension DTO {
+    struct HomeResponse: Codable {
+        /// 강의 데이터
+        let lecture: [DTO.Lecture]
+        /// 강의 데이터 갯수 - 불필요할 수 있음.
+        let count: Int
+    }
+}
