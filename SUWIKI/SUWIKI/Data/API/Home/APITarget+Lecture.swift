e5623188739acc4cc02cd7e99fcf867a858da2a5
sozohoy <sozohoy@gmail.com>
2024-01-25 18:11:51 +0900

    feat: 메인 API Target 구현(#38)

--- SUWIKI/SUWIKI/Data/API/Home/APITarget+Lecture.swift
+++ SUWIKI/SUWIKI/Data/API/Home/APITarget+Lecture.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Alamofire
+
+extension APITarget {
+    enum Lecture: TargetType {
+        case getHome(DTO.AllLectureRequest)
+    }
+}
+
+extension APITarget.Lecture {
+    var targetURL: URL {
+        URL(string: APITarget.baseURL + "lecture")!
+    }
+
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .getHome:
+            return .get
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .getHome:
+            "/all"
+        }
+    }
+
+    var parameters: RequestParameter {
+        switch self {
+        case let .getHome(allLectureRequest):
+            return .query(allLectureRequest)
+        }
+    }
+}
