1390e7a583a0f2c6d867d501c5134c9e50341c51
sozohoy <sozohoy@gmail.com>
2024-01-25 16:23:10 +0900

    feat: 파라미터 요청 종류 enum으로 분기(#38)

--- SUWIKI/SUWIKI/Core/Network/RequestParameter.swift
+++ SUWIKI/SUWIKI/Core/Network/RequestParameter.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+public enum RequestParameter {
+    case plain
+    case query(_ parameter: Encodable)
+    case body(_ parameter: Encodable)
+    case both(query: Encodable, json: Encodable)
+}
