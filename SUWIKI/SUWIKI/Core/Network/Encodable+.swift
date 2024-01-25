b5c4089c0a9d3e3116fead0be74d80622a540453
sozohoy <sozohoy@gmail.com>
2024-01-25 16:23:34 +0900

    feat: Encodable한 형태를 Dictionary로 변환하는 메소드(#38)

--- SUWIKI/SUWIKI/Core/Network/Encodable+.swift
+++ SUWIKI/SUWIKI/Core/Network/Encodable+.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension Encodable {
+    func toDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self),
+              let jsonData = try? JSONSerialization.jsonObject(with: data),
+              let dictionary = jsonData as? [String: Any] else { return [:] }
+        return dictionary
+    }
+}
