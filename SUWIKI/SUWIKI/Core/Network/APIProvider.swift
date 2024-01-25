edc9274d51b886b63dfd989563c924ee032edb76
sozohoy <sozohoy@gmail.com>
2024-01-25 18:10:10 +0900

    feat: API Provider 구현(#38)

--- SUWIKI/SUWIKI/Core/Network/APIProvider.swift
+++ SUWIKI/SUWIKI/Core/Network/APIProvider.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+import Alamofire
+
+class APIProvider {
+    static func request<T: Decodable>(
+        _ object: T.Type,
+        target: TargetType
+    ) async throws -> T {
+        return try await AlamofireManager
+            .shared
+            .session
+            .request(target)
+            .serializingDecodable()
+            .value
+    }
+}
