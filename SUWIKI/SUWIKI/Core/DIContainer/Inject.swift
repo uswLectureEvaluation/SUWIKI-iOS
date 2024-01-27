7a8eadce8cd2c82b415c12854e70e95dfcc762a0
sozohoy <sozohoy@gmail.com>
2024-01-27 18:28:52 +0900

    feat: Inject 프로퍼티 래퍼 구현(#38)

--- SUWIKI/SUWIKI/Core/DIContainer/Inject.swift
+++ SUWIKI/SUWIKI/Core/DIContainer/Inject.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+// MARK: - Inject, 추상화를 의존하는 경우에 이니셜라이저를 수정하지 않고 프로퍼티 래퍼로 의존성 주입
+@propertyWrapper
+public struct Inject<Service> {
+    public let wrappedValue: Service
+
+    public init() {
+        self.wrappedValue = DIContainer.shared.resolve(type: Service.self)
+    }
+}
