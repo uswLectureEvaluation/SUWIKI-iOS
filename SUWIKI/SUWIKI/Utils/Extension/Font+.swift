0820da1fb5ebc181dfdb4f420dd37d6915b729c0
sozohoy <sozohoy@gmail.com>
2024-01-29 18:32:24 +0900

    add: Font(#38)

---
+++ SUWIKI/SUWIKI/Utils/Extension/Font+.swift
@@ -0,0 +1,76 @@
+//
+//  Font+.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 1/29/24.
+//
+
+import SwiftUI
+
+extension SwiftUI.Font {
+    static var h1: Font {
+        Font.system(size: 22, weight: .bold)
+    }
+    static var h2: Font {
+        Font.system(size: 18, weight: .bold)
+    }
+    static var h3: Font {
+        Font.system(size: 18, weight: .medium)
+    }
+    static var h4: Font {
+        Font.system(size: 18, weight: .regular)
+    }
+    static var h5: Font {
+        Font.system(size: 16, weight: .bold)
+    }
+    static var h6: Font {
+        Font.system(size: 16, weight: .medium)
+    }
+    static var h7: Font {
+        Font.system(size: 16, weight: .regular)
+    }
+
+    static var b1: Font {
+        Font.system(size: 15, weight: .bold)
+    }
+    static var b2: Font {
+        Font.system(size: 15, weight: .medium)
+    }
+    static var b3: Font {
+        Font.system(size: 15, weight: .regular)
+    }
+    static var b4: Font {
+        Font.system(size: 14, weight: .medium)
+    }
+    static var b5: Font {
+        Font.system(size: 14, weight: .regular)
+    }
+    static var b6: Font {
+        Font.system(size: 13, weight: .medium)
+    }
+    static var b7: Font {
+        Font.system(size: 13, weight: .regular)
+    }
+
+    static var c1: Font {
+        Font.system(size: 12, weight: .medium)
+    }
+    static var c2: Font {
+        Font.system(size: 12, weight: .regular)
+    }
+    static var c3: Font {
+        Font.system(size: 11, weight: .bold)
+    }
+    static var c4: Font {
+        Font.system(size: 11, weight: .regular)
+    }
+    static var c5: Font {
+        Font.system(size: 10, weight: .medium)
+    }
+    static var c6: Font {
+        Font.system(size: 10, weight: .regular)
+    }
+    static var c7: Font {
+        Font.system(size: 8, weight: .regular)
+    }
+}
