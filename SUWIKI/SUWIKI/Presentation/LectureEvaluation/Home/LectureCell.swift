d699bad09664c7b4927739ef4231db3d2022e996
sozohoy <sozohoy@gmail.com>
2024-01-29 21:37:45 +0900

    feat: lectureCell(#38)

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Home/LectureCell.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Home/LectureCell.swift
@@ -8,67 +8,80 @@
 import SwiftUI
 
 struct LectureCell: View {
-
+    
     let lecture: Lecture
-
+    
     var body: some View {
-        RoundedRectangle(cornerSize: .init(width: 10, height: 10))
-            .foregroundStyle(Color.white)
-            .overlay(alignment: .topLeading) {
-                info
-            }
-            .frame(height: 100)
-            .padding(.horizontal, 24)
+        ZStack {
+            Color.gray
+            info
+        }
     }
-
+    
     var info: some View {
         VStack(alignment: .leading,
                spacing: 0) {
-            HStack(spacing: 0) {
-                Text(lecture.name)
-                    .font(.h3)
-                Spacer()
-                RoundedRectangle(cornerSize: CGSize(width: 10,
-                                                    height: 10))
-                .foregroundStyle(Color(uiColor: .grayF6))
-                .frame(width: 33, height: 21)
-                .overlay {
-                    Text(lecture.lectureType)
-                        .font(.c4)
-                        .foregroundStyle(Color(uiColor: .gray6A))
-                }
-            }
-            .padding(.top, 16)
-
-            HStack(spacing: 0) {
-                Text(lecture.major)
-                    .font(.b7)
-                Rectangle()
-                    .frame(width: 1, height: 14)
-                    .padding(.horizontal, 4)
-                Text(lecture.professor)
-                    .font(.b7)
-            }
-            .foregroundStyle(Color(uiColor: .gray6A))
-            .padding(.top, 4)
-            HStack(spacing: 0) {
-                Image(systemName: "star.fill")
-                    .frame(width: 16, height: 16)
-                    .padding(.top, 6)
-                    .padding(.trailing, 4)
-                Text(avgToString(lecture.lectureTotalAvg))
-                    .font(.b1)
-                    .padding(.top, 7)
-            }
-            .foregroundStyle(Color(uiColor: .primaryColor))
+            nameAndLectureType
+            majorAndProfessor
+            avarage
+            Spacer()
         }
                .padding(.leading, 14)
+               .frame(height: 100)
+               .background(Color.white)
     }
-
+    
+    var nameAndLectureType: some View {
+        HStack(spacing: 0) {
+            Text(lecture.name)
+                .font(.h3)
+                .foregroundStyle(Color(uiColor: .basicBlack))
+            Spacer()
+            RoundedRectangle(cornerSize: CGSize(width: 10,
+                                                height: 10))
+            .foregroundStyle(Color(uiColor: .grayF6))
+            .frame(width: 33, height: 21)
+            .overlay {
+                Text(lecture.lectureType)
+                    .font(.c4)
+                    .foregroundStyle(Color(uiColor: .gray6A))
+            }
+            .padding(.trailing, 18)
+        }
+        .padding(.top, 16)
+    }
+    
+    var majorAndProfessor: some View {
+        HStack(spacing: 0) {
+            Text(lecture.major)
+                .font(.b7)
+            Rectangle()
+                .frame(width: 1, height: 14)
+                .padding(.horizontal, 4)
+            Text(lecture.professor)
+                .font(.b7)
+        }
+        .foregroundStyle(Color(uiColor: .gray6A))
+        .padding(.top, 6)
+    }
+    
+    var avarage: some View {
+        HStack(spacing: 0) {
+            Image(systemName: "star.fill")
+                .frame(width: 16, height: 16)
+                .padding(.top, 7)
+                .padding(.trailing, 4)
+            Text(avgToString(lecture.lectureTotalAvg))
+                .font(.b1)
+                .padding(.top, 8)
+        }
+        .foregroundStyle(Color(uiColor: .primaryColor))
+    }
+    
     func avgToString(_ avg: Double) -> String {
         return String(format: "%.1f", avg)
     }
-
+    
 }
 
 #Preview {
