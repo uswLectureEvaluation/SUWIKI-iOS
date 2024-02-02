ba9612433d4dcbdc18e72c8e1186d44d61a0f346
sozohoy <sozohoy@gmail.com>
2024-02-02 17:04:01 +0900

    feat: 검색 유즈케이스 구현(#38)

--- SUWIKI/SUWIKI/Domain/UseCases/Lecture/SearchLectureUseCase.swift
+++ SUWIKI/SUWIKI/Domain/UseCases/Lecture/SearchLectureUseCase.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol SearchLectureUseCase {
+    func search(
+        searchText: String,
+        option: LectureOption,
+        page: Int,
+        major: String?
+    ) async throws -> [Lecture]
+}
+
+struct DefaultSearchLectureUseCase: SearchLectureUseCase {
+
+    @Inject var repository: LectureRepository
+
+    func search(
+        searchText: String,
+        option: LectureOption,
+        page: Int,
+        major: String?
+    ) async throws -> [Lecture] {
+        try await repository.search(searchText: searchText,
+                                    option: option,
+                                    page: page,
+                                    major: major)
+    }
+}
