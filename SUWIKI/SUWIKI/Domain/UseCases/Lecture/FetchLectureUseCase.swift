e6e0e62aa971e3135412483f22deec0b0a849e4f
sozohoy <sozohoy@gmail.com>
2024-02-04 03:29:47 +0900

    refactor: 유즈케이스 분리(#41)

--- SUWIKI/SUWIKI/Domain/UseCases/Lecture/FetchLectureUseCase.swift
+++ SUWIKI/SUWIKI/Domain/UseCases/Lecture/FetchLectureUseCase.swift
@@ -2,30 +2,7 @@
 //  FetchLectureUseCase.swift
 //  SUWIKI
 //
-//  Created by 한지석 on 1/27/24.
+//  Created by 한지석 on 2/4/24.
 //
 
 import Foundation
-
-protocol FetchLectureUseCase {
-    func fetch(
-        option: LectureOption,
-        page: Int,
-        major: String?
-    ) async throws -> [Lecture]
-}
-
-struct DefaultFetchLectureUseCase: FetchLectureUseCase {
-
-    @Inject var repository: LectureRepository
-
-    func fetch(
-        option: LectureOption,
-        page: Int,
-        major: String?
-    ) async throws -> [Lecture] {
-        try await repository.fetch(option: option,
-                                   page: page,
-                                   major: major)
-    }
-}
--- SUWIKI/SUWIKI/Domain/UseCases/Lecture/SearchLectureUseCase.swift
+++ SUWIKI/SUWIKI/Domain/UseCases/Lecture/SearchLectureUseCase.swift
@@ -2,33 +2,7 @@
 //  SearchLectureUseCase.swift
 //  SUWIKI
 //
-//  Created by 한지석 on 2/2/24.
+//  Created by 한지석 on 2/4/24.
 //
 
 import Foundation
-
-protocol SearchLectureUseCase {
-    func search(
-        searchText: String,
-        option: LectureOption,
-        page: Int,
-        major: String?
-    ) async throws -> [Lecture]
-}
-
-struct DefaultSearchLectureUseCase: SearchLectureUseCase {
-
-    @Inject var repository: LectureRepository
-
-    func search(
-        searchText: String,
-        option: LectureOption,
-        page: Int,
-        major: String?
-    ) async throws -> [Lecture] {
-        try await repository.search(searchText: searchText,
-                                    option: option,
-                                    page: page,
-                                    major: major)
-    }
-}
