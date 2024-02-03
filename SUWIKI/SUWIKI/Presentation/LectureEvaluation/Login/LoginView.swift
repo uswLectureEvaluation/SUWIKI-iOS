7a8464d35dbee8fec83bd9af3960051357ce6a15
sozohoy <sozohoy@gmail.com>
2024-02-04 03:31:18 +0900

    feat: 로그인 화면 구현(#41)

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/LoginView.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/LoginView.swift
@@ -7,30 +7,20 @@
 
 import SwiftUI
 
-enum LoginViewInputType: CaseIterable {
-    case id
-    case password
-}
-
-enum LoginViewButtonType: CaseIterable {
-    case findId
-    case findPassword
-    case signIn
-}
-
 struct LoginView: View {
 
-    @State var id: String = ""
-    @State var password: String = ""
+    @FocusState private var focusField: LoginViewInputType?
+    @StateObject var viewModel = LoginViewModel()
 
     var body: some View {
         VStack {
             title
             ForEach(LoginViewInputType.allCases, id: \.self) { input in
-                inputView(input)
+                inputViews(input)
             }
             Spacer()
-            eventBox
+            buttonViews
+            loginButton
         }
     }
 
@@ -40,57 +30,123 @@ struct LoginView: View {
             .padding(.top, 60)
     }
 
-    func inputView(
+    func inputViews(
         _ type: LoginViewInputType
     ) -> some View {
         VStack {
             HStack {
                 Text(type == .id ? "아이디" : "비밀번호")
                     .font(.c2)
-                    .foregroundStyle(Color(uiColor: .gray95))
+                    .foregroundStyle(focusField == type ?
+                                     Color(uiColor: .primaryColor) : Color(uiColor: .gray95))
                 Spacer()
             }
-            TextField(type == .id ?
-                      "아이디를 입력하세요." : "비밀번호를 입력하세요.",
-                      text: $id)
+            if type == .id {
+                HStack {
+                    TextField("아이디를 입력하세요.",
+                              text: $viewModel.id)
+                    .focused($focusField,
+                             equals: .id)
+                    //TODO: Clear Button
+                    Button {
+                        print("tlqkf")
+                    } label: {
+                        Image(systemName: "person")
+                    }
+                }
+
+            } else {
+                HStack {
+                    SecureField("비밀번호를 입력하세요",
+                                text: $viewModel.password)
+                    .focused($focusField,
+                             equals: .password)
+                    //TODO: Visible Button
+                    Button {
+                        print("tlqkf")
+                    } label: {
+                        Image(systemName: "person")
+                    }
+                }
+            }
             Rectangle()
                 .frame(height: 1)
-                .foregroundStyle(Color(uiColor: .gray95))
+                .foregroundStyle(focusField == type ?
+                                 Color(uiColor: .primaryColor) : Color(uiColor: .gray95))
+                .padding(.top, 4)
         }
         .padding(.top, type == .id ? 26 : 20)
         .padding(.horizontal, 24)
     }
 
-    var eventBox: some View {
+    var buttonViews: some View {
         HStack {
-            Text("아이디 찾기")
-                .font(.b5)
-                .foregroundStyle(Color(uiColor: .gray6A))
-            Rectangle()
-                .frame(width: 1, height: 12)
-                .foregroundStyle(Color(uiColor: .grayF6))
-            Text("비밀번호 찾기")
-                .font(.b5)
-                .foregroundStyle(Color(uiColor: .gray6A))
-            Rectangle()
-                .frame(width: 1, height: 12)
-                .foregroundStyle(Color(uiColor: .grayF6))
-            Text("회원가입")
-                .font(.b5)
-                .foregroundStyle(Color(uiColor: .primaryColor))
+            ForEach(LoginViewButtonType.allCases, id: \.self) { type in
+                buttons(type)
+            }
         }
     }
 
     func buttons(
         _ type: LoginViewButtonType
     ) -> some View {
-        Button {
-            
-        } label: {
-
+        HStack {
+            Button {
+                print(type)
+            } label: {
+                Text(type.title)
+                    .font(.b5)
+                    .foregroundStyle(Color(uiColor: type == .signIn ? .primaryColor : .gray6A))
+            }
+            if type != .signIn {
+                Rectangle()
+                    .frame(width: 1, height: 12)
+                    .foregroundStyle(Color(uiColor: .grayF6))
+            }
         }
     }
 
+    var loginButton: some View {
+        Button {
+            /// Login API 붙히기
+            print("@LOG login")
+        } label: {
+            RoundedRectangle(cornerRadius: 15.0)
+                .frame(height: 50)
+                .overlay {
+                    Text("로그인")
+                        .font(.h5)
+                        .foregroundStyle(Color(uiColor: .white))
+                }
+        }
+        .disabled(viewModel.isButtonDisabled)
+        .padding(.horizontal, 24)
+        .padding(.top, 16)
+        .padding(.bottom, 120)
+    }
+
+}
+
+enum LoginViewInputType: CaseIterable {
+    case id
+    case password
+}
+
+enum LoginViewButtonType: CaseIterable {
+    case findId
+    case findPassword
+    case signIn
+
+    var title: String {
+        switch self {
+        case .findId:
+            "아이디 찾기"
+        case .findPassword:
+            "비밀번호 찾기"
+        case .signIn:
+            "회원가입"
+        }
+    }
 }
 
 #Preview {
