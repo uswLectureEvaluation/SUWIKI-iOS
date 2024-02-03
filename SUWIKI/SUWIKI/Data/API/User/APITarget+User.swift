b263de54f48eeb8ee692086dfb83876382e4c7c4
sozohoy <sozohoy@gmail.com>
2024-02-04 03:30:05 +0900

    feat: 회원가입, 아이디 - 이메일 중복확인 연결(#41)

--- SUWIKI/SUWIKI/Data/API/User/APITarget+User.swift
+++ SUWIKI/SUWIKI/Data/API/User/APITarget+User.swift
@@ -12,6 +12,9 @@ import Alamofire
 extension APITarget {
     enum User: TargetType {
         case login(DTO.LoginRequest)
+        case join(DTO.JoinRequest)
+        case checkId(DTO.CheckIdRequest)
+        case checkEmail(DTO.CheckEmailRequest)
     }
 }
 
@@ -24,6 +27,12 @@ extension APITarget.User {
         switch self {
         case .login:
             return .post
+        case .join:
+            return .post
+        case .checkId:
+            return .post
+        case .checkEmail:
+            return .post
         }
     }
 
@@ -31,6 +40,12 @@ extension APITarget.User {
         switch self {
         case .login:
             return "/login"
+        case .join:
+            return "/join"
+        case .checkId:
+            return "/check-id"
+        case .checkEmail:
+            return "/check-email"
         }
     }
 
@@ -38,6 +53,12 @@ extension APITarget.User {
         switch self {
         case let .login(loginRequest):
             return .body(loginRequest)
+        case let .join(joinRequest):
+            return .body(joinRequest)
+        case let .checkId(checkIdRequest):
+            return .body(checkIdRequest)
+        case let .checkEmail(checkEmailRequest):
+            return .body(checkEmailRequest)
         }
     }
 }
