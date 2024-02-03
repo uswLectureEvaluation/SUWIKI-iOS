1b4d71da45690e8aa9cdbe2f094ac88a1d06b80b
sozohoy <sozohoy@gmail.com>
2024-02-04 03:31:38 +0900

    feat: LoginVM(#41)

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/LoginViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/LoginViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import Combine
+
+/// keychain 저장
+/// 로그인 유즈케이스
+final class LoginViewModel: ObservableObject {
+    
+//    var useCase: AuthUseCase = DIContainer.shared.resolve(type: AuthUseCase.self)
+    @Published var id: String = ""
+    @Published var password: String = ""
+    @Published var isButtonDisabled = true
+    var cancellables = Set<AnyCancellable>()
+    lazy var vaildLoginButton: AnyPublisher<Bool, Never> = Publishers.CombineLatest($id, $password)
+        .map {
+            return $0.count < 6 || $0.count > 20 || $1.count < 8 || $1.count > 20
+        }
+        .removeDuplicates()
+        .eraseToAnyPublisher()
+    
+    init() {
+        vaildLoginButton
+            .receive(on: RunLoop.main)
+            .sink { self.isButtonDisabled = $0 }
+            .store(in: &cancellables)
+    }
+    
+}
+
+//id == 6, 20
+//pwd == 8, 20 영, 숫, 특수문자
+
