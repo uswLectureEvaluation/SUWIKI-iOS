7c249d89d2590503dfae86f2553f25061a67ddd3
sozohoy <sozohoy@gmail.com>
2024-02-06 19:05:25 +0900

    feat: 인터셉터 구현, 토큰 재발급 구현 예정(#43)

--- SUWIKI/SUWIKI/Core/Network/Interceptor.swift
+++ SUWIKI/SUWIKI/Core/Network/Interceptor.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+import Alamofire
+
+final class BaseInterceptor: RequestInterceptor {
+
+    let keychainManager = KeychainManager.shared
+
+    func adapt(
+        _ urlRequest: URLRequest,
+        for session: Session,
+        completion: @escaping (Result<URLRequest, Error>) -> Void) {
+            var request = urlRequest
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            if let accessToken = keychainManager.read(token: .AccessToken) {
+                request.setValue(accessToken, forHTTPHeaderField: "Authorization")
+            }
+            completion(.success(request))
+        }
+
+//    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+//        <#code#>
+//    }
+}
