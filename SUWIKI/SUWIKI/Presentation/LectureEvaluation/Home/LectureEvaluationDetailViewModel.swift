0571ac34e8de426b63a97a99c6cfdaea1d0bdc92
sozohoy <sozohoy@gmail.com>
2024-02-06 19:07:56 +0900

    feat: 강의평가 상세 API 호출(#43)

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Home/LectureEvaluationDetailViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Home/LectureEvaluationDetailViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+//4591
+final class LectureEvaluationDetailViewModel: ObservableObject {
+    var fetchDetailLectureUseCase: FetchDetailLectureUseCase = DIContainer.shared.resolve(type: FetchDetailLectureUseCase.self)
+    @Published var detailLecture: DetailLecture = DetailLecture.mockdata
+
+    @MainActor
+    init() {
+        Task {
+            do {
+                self.detailLecture = try await fetchDetailLectureUseCase.excute(id: 4591)
+            } catch {
+                print("@Log LectureEvaluationDetialVM Fetch - \(error.localizedDescription)")
+            }
+        }
+    }
+}
