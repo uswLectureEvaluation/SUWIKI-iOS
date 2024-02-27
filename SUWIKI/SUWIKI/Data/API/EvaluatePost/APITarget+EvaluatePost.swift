9f8a8767f105b5b25f4b68e4174289f687dd7462
sozohoy <sozohoy@gmail.com>
2024-02-27 18:40:24 +0900

    add: Files(#43)

---
+++ SUWIKI/SUWIKI/Data/API/EvaluatePost/APITarget+EvaluatePost.swift
@@ -0,0 +1,8 @@
+//
+//  APITarget+EvaluatePost.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2/27/24.
+//
+
+import Foundation
---
+++ SUWIKI/SUWIKI/Data/DTO/User/Response/DTO+RefreshResponse.swift
@@ -0,0 +1,8 @@
+//
+//  DTO+RefreshResponse.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2/27/24.
+//
+
+import Foundation
