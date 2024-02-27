a548d360222cbe546d0bfab52e1bb4f2d85e532b
sozohoy <sozohoy@gmail.com>
2024-02-27 18:45:19 +0900

    feat: 강의평가 글 내려받기 추가(#43)

--- SUWIKI/SUWIKI/Data/API/EvaluatePost/APITarget+EvaluatePost.swift
+++ SUWIKI/SUWIKI/Data/API/EvaluatePost/APITarget+EvaluatePost.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Alamofire
+
+extension APITarget {
+    enum EvaluatePost: TargetType {
+        case fetchEvaluatePosts(DTO.FetchEvaluatePostRequest)
+    }
+}
+
+extension APITarget.EvaluatePost {
+    var targetURL: URL {
+        URL(string: APITarget.baseURL + "evaluate-posts")!
+    }
+
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .fetchEvaluatePosts:
+            return .get
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .fetchEvaluatePosts:
+            ""
+        }
+    }
+
+    var parameters: RequestParameter {
+        switch self {
+        case let .fetchEvaluatePosts(fetchEvaluatePostRequest):
+            return .query(fetchEvaluatePostRequest)
+        }
+    }
+}
