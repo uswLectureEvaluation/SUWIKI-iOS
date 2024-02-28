240ead3d058b93e978ae74e2f63df46480d4928f
sozohoy <sozohoy@gmail.com>
2024-02-28 16:40:31 +0900

    feat: 인터셉터 리펙토링(#43)

--- SUWIKI/SUWIKI/Core/Network/Interceptor.swift
+++ SUWIKI/SUWIKI/Core/Network/Interceptor.swift
@@ -9,43 +9,81 @@ import Foundation
 
 import Alamofire
 
+//final class tempInterceptor: RequestInterceptor {
+//    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+//        <#code#>
+//    }
+//}
+
 final class BaseInterceptor: RequestInterceptor {
 
     let keychainManager = KeychainManager.shared
 
-    func adapt(
-        _ urlRequest: URLRequest,
-        for session: Session,
-        completion: @escaping (Result<URLRequest, Error>) -> Void) {
-            var request = urlRequest
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            if let accessToken = keychainManager.read(token: .AccessToken) {
-                request.setValue(accessToken, forHTTPHeaderField: "Authorization")
-            }
-            completion(.success(request))
+    func adapt(_ urlRequest: URLRequest, 
+               for session: Session,
+               completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var request = urlRequest
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        if let accessToken = keychainManager.read(token: .AccessToken) {
+            request.setValue(accessToken, forHTTPHeaderField: "Authorization")
         }
+        completion(.success(request))
+    }
 
-    func retry(
-        _ request: Request,
-        for session: Session,
-        dueTo error: Error,
-        completion: @escaping (RetryResult) -> Void) {
-            guard let accessToken = keychainManager.read(token: .AccessToken)
-            else {
-                completion(.doNotRetry)
-                return
-            }
-            let target = APITarget.User.refresh(DTO.RefreshRequest(authorization: accessToken))
-            Task {
-                do {
-                    let tokens = try await APIProvider.request(DTO.RefreshResponse.self,
-                                                               target: target)
-                    keychainManager.create(token: .AccessToken, value: tokens.accessToken)
-                    keychainManager.create(token: .RefreshToken, value: tokens.refreshToken)
-                    completion(.retry)
-                } catch {
-                    completion(.doNotRetry)
-                }
-            }
-        }
+//    func adapt(
+//        _ urlRequest: URLRequest,
+//        for session: Session,
+//        completion: @escaping (Result<URLRequest, Error>) -> Void) {
+//            var request = urlRequest
+//            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//            if let accessToken = keychainManager.read(token: .AccessToken) {
+//                request.setValue(accessToken, forHTTPHeaderField: "Authorization")
+//            }
+//            completion(.success(request))
+//        }
+
+//    func retry(_ request: Request, for session: Session, dueTo error: Error) async throws -> RetryResult {
+//        print("@LOG RETRY")
+//        guard let accessToken = keychainManager.read(token: .AccessToken) else {
+//            return .doNotRetry
+//        }
+//        let target = APITarget.User.refresh(DTO.RefreshRequest(authorization: accessToken))
+//        do {
+//
+//            let tokens = try await APIProvider.request(DTO.RefreshResponse.self, target: target)
+//            keychainManager.create(token: .AccessToken, value: tokens.accessToken)
+//            keychainManager.create(token: .RefreshToken, value: tokens.refreshToken)
+//            return .retry
+//        } catch {
+//            return .doNotRetryWithError(error)
+//        }
+//    }
+//    func retry(
+//        _ request: Request,
+//        for session: Session,
+//        dueTo error: Error,
+//        completion: @escaping (RetryResult) -> Void) {
+//            print("@LOG RETRY 2")
+//            guard let refreshToken = keychainManager.read(token: .RefreshToken)
+//            else {
+//                completion(.doNotRetry)
+//                return
+//            }
+//            let target = APITarget.User.refresh(DTO.RefreshRequest(authorization: refreshToken))
+//            Task {
+//                do {
+//                    let tokens = try await APIProvider.request(DTO.RefreshResponse.self, target: target)
+//                    keychainManager.create(token: .AccessToken, value: tokens.accessToken)
+//                    keychainManager.create(token: .RefreshToken, value: tokens.refreshToken)
+//                    await MainActor.run {
+//                        completion(.doNotRetry)
+//                    }
+//                } catch {
+//                    await MainActor.run {
+//                        completion(.doNotRetry)
+//                    }
+//                }
+//            }
+//        }
 }
+
---
+++ SUWIKI/SUWIKI/Core/Network/Temp.swift
@@ -0,0 +1,8 @@
+//
+//  Temp.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 2/27/24.
+//
+
+import Foundation
