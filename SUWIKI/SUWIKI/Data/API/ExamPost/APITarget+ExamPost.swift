92056e3e22377796c442c6256de34b03212aab16
sozohoy <sozohoy@gmail.com>
2024-03-01 21:24:25 +0900

    feat: Exam Api Target(#43)

--- SUWIKI/SUWIKI/Data/API/ExamPost/APITarget+ExamPost.swift
+++ SUWIKI/SUWIKI/Data/API/ExamPost/APITarget+ExamPost.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Alamofire
+
+extension APITarget {
+    enum ExamPost: TargetType {
+        case fetchExamPosts(DTO.FetchExamPostsRequest)
+    }
+}
+
+extension APITarget.ExamPost {
+    var targetURL: URL {
+        URL(string: APITarget.baseURL + "exam-posts")!
+    }
+
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .fetchExamPosts:
+            return .get
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .fetchExamPosts:
+            ""
+        }
+    }
+
+    var parameters: RequestParameter {
+        switch self {
+        case let .fetchExamPosts(fetchExamPosts):
+            return .query(fetchExamPosts)
+        }
+    }
+}
