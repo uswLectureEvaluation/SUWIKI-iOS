185e90a526533f9a00f6408e048a58b3b4e974dc
sozohoy <sozohoy@gmail.com>
2024-03-01 21:20:27 +0900

    add: emptyView(#43)

---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Detail/IsPostEmptyView.swift
@@ -0,0 +1,18 @@
+//
+//  IsPostEmptyView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 3/1/24.
+//
+
+import SwiftUI
+
+struct IsPostEmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IsPostEmptyView()
+}
