cc05a4c5e3308bde7996e636a03b3d3285104a28
sozohoy <sozohoy@gmail.com>
2024-03-01 19:48:44 +0900

    feat: DetailViewModel(#43)

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Detail/LectureEvaluationDetailViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Detail/LectureEvaluationDetailViewModel.swift
@@ -7,18 +7,25 @@
 
 import Foundation
 
+enum PostType {
+    case evaluate
+    case exam
+}
 //4591
 final class LectureEvaluationDetailViewModel: ObservableObject {
     var fetchDetailLectureUseCase: FetchDetailLectureUseCase = DIContainer.shared.resolve(type: FetchDetailLectureUseCase.self)
     var fetchEvaluatePostUseCase: FetchEvaluatePostsUseCase = DIContainer.shared.resolve(type: FetchEvaluatePostsUseCase.self)
+    var id: Int
+    @Published var postType: PostType = .evaluate
     @Published var detailLecture: DetailLecture = DetailLecture.mockdata
-    @Published var evaluatePosts: [EvaluatePost] = []
+    @Published var evaluatePosts: [EvaluatePost] = EvaluatePost.mockData
 
-    init() {
+    init(id: Int) {
+        self.id = id
         Task {
             do {
-                let detailLecture = try await fetchDetailLectureUseCase.excute(id: 3568)
-                let evaluatePosts = try await fetchEvaluatePostUseCase.execute(lectureId: 3568, page: 1)
+                let detailLecture = try await fetchDetailLectureUseCase.excute(id: id)
+                let evaluatePosts = try await fetchEvaluatePostUseCase.execute(lectureId: id, page: 1)
                 await MainActor.run {
                     self.detailLecture = detailLecture
                     self.evaluatePosts = evaluatePosts
