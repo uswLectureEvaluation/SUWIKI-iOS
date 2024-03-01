fbf3164a838d9334c9e607d4387452e445c9b2af
sozohoy <sozohoy@gmail.com>
2024-03-01 21:23:31 +0900

    feat: 시험정보 fetch 유즈케이스(#43)

--- SUWIKI/SUWIKI/Domain/UseCases/ExamPost/FetchExamPostsUseCase.swift
+++ SUWIKI/SUWIKI/Domain/UseCases/ExamPost/FetchExamPostsUseCase.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+protocol FetchExamPostsUseCase {
+    func execute(
+        id: Int,
+        page: Int
+    ) async throws -> ExamPostInfo
+}
+
+final class DefaultFetchExamPostUseCase: FetchExamPostsUseCase {
+    @Inject var repository: ExamPostRepository
+
+    func execute(id: Int, page: Int) async throws -> ExamPostInfo {
+        try await repository.fetch(id: id, page: page)
+    }
+}
