6a4e8551eee2c63c8a1b3012db552d34aa9a4cee
sozohoy <sozohoy@gmail.com>
2024-03-03 18:33:36 +0900

    feat: Equatable

--- SUWIKI/SUWIKI/Core/State/RequestState.swift
+++ SUWIKI/SUWIKI/Core/State/RequestState.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum RequestState: CustomState {
+    case notRequest
+    case isProgress
+    case success
+    case failed(Error)
+}
+
+extension RequestState {
+    static func == (lhs: RequestState, rhs: RequestState) -> Bool {
+        switch (lhs, rhs) {
+        case (.notRequest, .notRequest),
+             (.isProgress, .isProgress),
+             (.success, .success):
+            return true
+        case (.failed, .failed):
+            return true
+        default:
+            return false
+        }
+    }
+}
