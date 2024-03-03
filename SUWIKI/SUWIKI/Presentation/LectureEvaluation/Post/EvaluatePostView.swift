1cd3d14a2fa45170819c135cc72d4de0463b0d91
sozohoy <sozohoy@gmail.com>
2024-03-03 20:24:04 +0900

    add: files

---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Post/EvaluatePostView.swift
@@ -0,0 +1,18 @@
+//
+//  EvaluatePostView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 3/3/24.
+//
+
+import SwiftUI
+
+struct EvaluatePostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EvaluatePostView()
+}
---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Post/PostEnum.swift
@@ -0,0 +1,8 @@
+//
+//  PostEnum.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 3/3/24.
+//
+
+import Foundation
