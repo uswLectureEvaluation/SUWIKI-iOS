c79ec725b9ea9176ff1f27c5179a3925124cd14a
sozohoy <sozohoy@gmail.com>
2024-03-03 18:35:05 +0900

    feat: 데이터 없을 경우 출력

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Detail/IsPostEmptyView.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Detail/IsPostEmptyView.swift
@@ -12,7 +12,55 @@ struct IsPostEmptyView: View {
     @Binding var postType: PostType
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if postType == .evaluate {
+            evaluateEmpty
+        } else {
+            examEmpty
+        }
+    }
+
+    var evaluateEmpty: some View {
+        VStack(spacing: 0) {
+            Text("첫 강의평가를 작성하고\n10포인트를 획득하세요.")
+                .foregroundStyle(Color(uiColor: .gray95))
+                .font(.h4)
+                .lineSpacing(4)
+                .padding(.bottom, 28)
+            Button {
+                
+            } label: {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 158, height: 40)
+                    .foregroundStyle(Color(uiColor: .primaryColor))
+                    .overlay {
+                        Text("강의평가 작성하기")
+                            .font(.b2)
+                            .foregroundStyle(Color(uiColor: .white))
+                    }
+            }
+        }
+    }
+
+    var examEmpty: some View {
+        VStack(spacing: 0) {
+            Text("첫 시험정보를 작성하고\n10포인트를 획득하세요.")
+                .foregroundStyle(Color(uiColor: .gray95))
+                .font(.h4)
+                .lineSpacing(4)
+                .padding(.bottom, 28)
+            Button {
+
+            } label: {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 158, height: 40)
+                    .foregroundStyle(Color(uiColor: .primaryColor))
+                    .overlay {
+                        Text("시험정보 작성하기")
+                            .font(.b2)
+                            .foregroundStyle(Color(uiColor: .white))
+                    }
+            }
+        }
     }
 }
 
