c4e21de52ed3530ffeab06772e0229bb53d3d732
sozohoy <sozohoy@gmail.com>
2024-03-04 19:24:35 +0900

    feat: 강의평가 작성 유즈케이스

--- SUWIKI/SUWIKI/Domain/UseCases/EvaluatePost/WriteEvaluatePostUseCase.swift
+++ SUWIKI/SUWIKI/Domain/UseCases/EvaluatePost/WriteEvaluatePostUseCase.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+protocol WriteEvaluatePostUseCase {
+    func execute(
+        id: Int,
+        lectureName: String,
+        professor: String,
+        selectedSemester: String,
+        satisfaction: Double,
+        learning: Double,
+        honey: Double,
+        team: Int,
+        difficulty: Int,
+        homework: Int,
+        content: String
+    ) async throws -> Bool
+}
+
+final class DefaultWriteEvaluatePostUseCase: WriteEvaluatePostUseCase {
+
+    @Inject var repository: EvaluatePostRepository
+
+    func execute(id: Int,
+                 lectureName: String,
+                 professor: String,
+                 selectedSemester: String,
+                 satisfaction: Double,
+                 learning: Double,
+                 honey: Double,
+                 team: Int,
+                 difficulty: Int,
+                 homework: Int,
+                 content: String
+    ) async throws -> Bool {
+        try await repository.write(id: id,
+                                   lectureName: lectureName,
+                                   professor: professor,
+                                   selectedSemester: selectedSemester,
+                                   satisfaction: satisfaction,
+                                   learning: learning,
+                                   honey: honey,
+                                   team: team,
+                                   difficulty: difficulty,
+                                   homework: homework,
+                                   content: content)
+    }
+}
