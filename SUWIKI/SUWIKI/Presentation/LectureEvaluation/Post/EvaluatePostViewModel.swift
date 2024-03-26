4a556ef34b111f4e397199fd724a3402877f316e
sozohoy <sozohoy@gmail.com>
2024-03-26 17:07:35 +0900

    feat: 데이터 combineLatest 적용

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Post/EvaluatePostViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Post/EvaluatePostViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 final class EvaluatePostViewModel: ObservableObject {
 
@@ -22,6 +23,9 @@ final class EvaluatePostViewModel: ObservableObject {
     @Published var homeworkType: HomeworkType = .notSelected
     @Published var teamplayType: TeamplayType = .notSelected
     @Published var content: String = ""
+    @Published var averagePoint: Double = 2.5
+    @Published var isWriteEnabled = false
+    var cancellables = Set<AnyCancellable>()
 
     init(
         id: Int,
@@ -35,19 +39,49 @@ final class EvaluatePostViewModel: ObservableObject {
         let splitedSemester = semester.split(separator: ", ").map { String($0) }
         self.semesterList = splitedSemester
         self.selectedSemester = splitedSemester[0]
+
+        $honeyPoint
+            .combineLatest($learningPoint, $satisfactionPoint)
+            .sink { _ in
+                self.setAveragePoint()
+            }
+            .store(in: &cancellables)
+
+        $difficultyType
+            .combineLatest($homeworkType, $teamplayType, $content)
+            .map { difficultyType, homeworkType, teamplayType, content in
+                return difficultyType != .notSelected && homeworkType != .notSelected && teamplayType != .notSelected && !content.isEmpty
+            }
+            .filter { $0 == true }
+            .sink {
+                self.isWriteEnabled = $0
+            }
+            .store(in: &cancellables)
     }
 
     func write() async throws {
-        print(try await writeEvaluatePostUseCase.execute(id: self.id,
-                                                         lectureName: self.lectureName,
-                                                         professor: self.professor,
-                                                         selectedSemester: self.selectedSemester,
-                                                         satisfaction: self.satisfactionPoint,
-                                                         learning: self.learningPoint,
-                                                         honey: self.honeyPoint,
-                                                         team: self.teamplayType.point,
-                                                         difficulty: self.difficultyType.point,
-                                                         homework: self.homeworkType.point,
-                                                         content: self.content))
+        if isWriteEnabled {
+            if try await writeEvaluatePostUseCase.execute(id: self.id,
+                                                              lectureName: self.lectureName,
+                                                              professor: self.professor,
+                                                              selectedSemester: self.selectedSemester,
+                                                              satisfaction: self.satisfactionPoint,
+                                                              learning: self.learningPoint,
+                                                              honey: self.honeyPoint,
+                                                              team: self.teamplayType.point,
+                                                              difficulty: self.difficultyType.point,
+                                                              homework: self.homeworkType.point,
+                                                              content: self.content) {
+                    // dismiss
+                } else {
+                    // 이미 글 작성함
+                }
+        } else {
+            print("데이터 누락")
+        }
+    }
+
+    func setAveragePoint() {
+        self.averagePoint = (honeyPoint + learningPoint + satisfactionPoint) / 3
     }
 }
