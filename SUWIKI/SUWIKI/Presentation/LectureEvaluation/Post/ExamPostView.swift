e74c8db1fa3a8d785d952b5f34460279023f5fdd
sozohoy <sozohoy@gmail.com>
2024-03-28 14:44:37 +0900

    add: 시험정보 작성 기능

---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Post/ExamPostView.swift
@@ -0,0 +1,18 @@
+//
+//  ExamPostView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 3/28/24.
+//
+
+import SwiftUI
+
+struct ExamPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExamPostView()
+}
---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Post/ExamPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ExamPostViewModel.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 3/28/24.
+//
+
+import Foundation
