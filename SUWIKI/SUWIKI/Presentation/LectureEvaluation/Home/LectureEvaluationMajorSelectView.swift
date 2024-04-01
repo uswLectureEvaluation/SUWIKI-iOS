eac66fe1c6d160dcaa9dd4376b04cd1f8bc93ca1
sozohoy <sozohoy@gmail.com>
2024-04-01 18:59:17 +0900

    add: 학과 선택 뷰

---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Home/LectureEvaluationMajorSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  LectureEvaluationMajorSelectView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 3/31/24.
+//
+
+import SwiftUI
+
+struct LectureEvaluationMajorSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LectureEvaluationMajorSelectView()
+}
---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Home/LectureEvaluationMajorSelectViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LectureEvaluationMajorSelectViewModel.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 3/31/24.
+//
+
+import Foundation
