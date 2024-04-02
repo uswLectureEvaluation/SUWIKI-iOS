0daa4d3a1c858d97461ec62cc7203e15cc92813a
sozohoy <sozohoy@gmail.com>
2024-04-02 16:33:41 +0900

    feat: timtable UIViewControllerRepresentable 구현

--- SUWIKI/SUWIKI/Presentation/TabBar/TimetableWrapperView.swift
+++ SUWIKI/SUWIKI/Presentation/TabBar/TimetableWrapperView.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TimetableWrapperView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let timetableStoryboard = UIStoryboard(name: "TimetableView", bundle: .main)
+        let timetableController = timetableStoryboard.instantiateViewController(withIdentifier: "timetableVC") as! TimetableViewController
+        return UINavigationController(rootViewController: timetableController)
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+}
