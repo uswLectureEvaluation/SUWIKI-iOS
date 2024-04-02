afab1914b47272da4bd612d2250b99c8f8512085
sozohoy <sozohoy@gmail.com>
2024-04-02 16:33:22 +0900

    add: TabBar 구현하기 위해 WrapperView & TabBarView

---
+++ SUWIKI/SUWIKI/Presentation/TabBar/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 4/2/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
---
+++ SUWIKI/SUWIKI/Presentation/TabBar/TimetableWrapperView.swift
@@ -0,0 +1,8 @@
+//
+//  TimetableWrapperView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 4/2/24.
+//
+
+import Foundation
