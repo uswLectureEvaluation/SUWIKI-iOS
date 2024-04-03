d18a20e2430424defc367f3f6e438d24d9a42558
sozohoy <sozohoy@gmail.com>
2024-04-03 21:18:52 +0900

    add: 공지사항 Entity

---
+++ SUWIKI/SUWIKI/Domain/Entity/LectureEvaluation/Announcement/Announcement.swift
@@ -0,0 +1,27 @@
+//
+//  Announcement.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 4/3/24.
+//
+
+import Foundation
+
+struct Announcement {
+    let id: Int
+    let title: String
+    let date: String
+    let content: String?
+
+    init(
+        id: Int,
+        title: String,
+        date: String,
+        content: String? = nil
+    ) {
+        self.id = id
+        self.title = title
+        self.date = date
+        self.content = content
+    }
+}
