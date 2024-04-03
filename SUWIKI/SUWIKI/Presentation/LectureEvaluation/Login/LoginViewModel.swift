07192b7a315aae9c06559807622e2655045141b9
sozohoy <sozohoy@gmail.com>
2024-04-03 21:18:00 +0900

    feat: 로직 수정

--- SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/LoginViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/LoginViewModel.swift
@@ -9,9 +9,9 @@ import Foundation
 import Combine
 
 final class LoginViewModel: ObservableObject {
-    
-    var signInUseCase: SignInUseCase = DIContainer.shared.resolve(type: SignInUseCase.self)
-    var createTokenUseCase: CreateTokenUseCase = DIContainer.shared.resolve(type: CreateTokenUseCase.self)
+
+    @Inject var respository: UserRepository
+
     @Published var id: String = "" {
         didSet {
             isInvalid = true
@@ -27,6 +27,7 @@ final class LoginViewModel: ObservableObject {
     @Published var isPasswordVisible = false
     var cancellables = Set<AnyCancellable>()
     lazy var vaildLoginButton: AnyPublisher<Bool, Never> = Publishers.CombineLatest($id, $password)
+        .receive(on: RunLoop.main)
         .map {
             return $0.count < 6 || $0.count > 20 || $1.count < 8 || $1.count > 20
         }
@@ -43,15 +44,12 @@ final class LoginViewModel: ObservableObject {
     /// func signIn: signIn Usecase를 실행한 후 리턴받은 토큰을 키체인에 저장합니다.
     /// signInUseCase & createTokenUseCase를 실행합니다.
     /// 로그인에 실패했을 경우 catch문을 통해 isInvalid 프로퍼티를 true, UI 피드백을 진행합니다.
-    func signIn() async throws {
+    func signIn() async throws -> Bool {
         do {
-            let signIn = try await signInUseCase.excute(id: self.id, password: self.password)
-            createTokenUseCase.excute(token: .AccessToken, value: signIn.accessToken)
-            createTokenUseCase.excute(token: .RefreshToken, value: signIn.refreshToken)
+            return try await self.respository.login(id: self.id, password: self.password)
         } catch {
             isInvalid = true
-            print("에러 캐치")
+            return false
         }
     }
-
 }
