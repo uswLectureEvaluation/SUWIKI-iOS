754cee88e990756ebf8b0c5ceb647bac896722da
sozohoy <sozohoy@gmail.com>
2024-04-04 19:50:11 +0900

    feat: 공지사항 레포지토리 구현

--- SUWIKI/SUWIKI/Data/Repositories/DefaultNoticeRepository.swift
+++ SUWIKI/SUWIKI/Data/Repositories/DefaultNoticeRepository.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class DefaultNoticeRepository: NoticeRepository {
+    func fetch() async throws -> [Announcement] {
+        let apiTarget = APITarget
+            .Notice
+            .fetchAnnouncements
+        let value = try await APIProvider.request(DTO.FetchAnnouncementResponse.self,
+                                                  target: apiTarget)
+        return value.announcements.map { $0.entity }
+    }
+
+    func fetchDetail(id: Int) async throws -> Announcement {
+        let apiTarget = APITarget
+            .Notice
+            .fetchDetailAnnouncement(
+                DTO.FetchDetailAnnouncementRequest(noticeId: id)
+            )
+        let value = try await APIProvider.request(DTO.FetchDetailAnnouncementResponse.self,
+                                                  target: apiTarget)
+        return value.announcement.entity
+    }
+}
