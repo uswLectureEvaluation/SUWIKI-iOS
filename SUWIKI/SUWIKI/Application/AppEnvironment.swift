c3f943b9464066bf154ca35c4554be50ea049277
sozohoy <sozohoy@gmail.com>
2024-04-05 19:46:46 +0900

    feat: DI

--- SUWIKI/SUWIKI/Application/AppEnvironment.swift
+++ SUWIKI/SUWIKI/Application/AppEnvironment.swift
@@ -28,24 +28,51 @@ struct AppEnvironment {
                            DefaultUserRepository())
         container.register(type: KeychainRepository.self,
                            DefaultKeychainRepository())
-        container.register(type: EvaluatePostRepository.self,
-                           DefaultEvaluatePostRepository())
+        container.register(type: EvaluationPostRepository.self,
+                           DefaultEvaluationPostRepository())
         container.register(type: ExamPostRepository.self,
                            DefaultExamPostRepository())
+        container.register(type: NoticeRepository.self,
+                           DefaultNoticeRepository())
     }
 
     func registerUseCases() {
+        /// 강의평가 내려받기
         container.register(type: FetchLectureUseCase.self, DefaultFetchLectureUseCase())
+        /// 강의평가 검색
         container.register(type: SearchLectureUseCase.self, DefaultSearchLectureUseCase())
-        container.register(type: SignInUseCase.self, DefaultSignInUseCase())
+        /// 회원가입
+//        container.register(type: SignInUseCase.self, DefaultSignInUseCase())
+        /// 토큰 생성
         container.register(type: CreateTokenUseCase.self, DefaultCreateTokenUseCase())
+        /// 토큰 읽기
         container.register(type: ReadTokenUseCase.self, DefaultReadTokenUseCase())
+        /// 강의평가 요약 내려받기
         container.register(type: FetchDetailLectureUseCase.self, DefaultFetchDetailLectureUseCase())
-        container.register(type: FetchEvaluatePostsUseCase.self, DefaultFetchEvaluatePostsUseCase())
+        /// 강의평가 상세 내려받기
+        container.register(type: FetchEvaluationPostsUseCase.self, DefaultFetchEvaluationPostsUseCase())
+        /// 시험정보 상세 내려받기
         container.register(type: FetchExamPostsUseCase.self, DefaultFetchExamPostUseCase())
-        container.register(type: WriteEvaluatePostUseCase.self, DefaultWriteEvaluatePostUseCase())
-        container.register(type: WriteExamPostUseCase.self,
-                           DefaultWriteExamPostUseCase())
+        /// 강의평가 작성
+        container.register(type: WriteEvaluationPostUseCase.self, DefaultWriteEvaluationPostUseCase())
+        /// 시험정보 작성
+        container.register(type: WriteExamPostUseCase.self,DefaultWriteExamPostUseCase())
+        /// 시험정보 구매
+        container.register(type: PurchaseExamPostUseCase.self, DefaultPurchaseExamPostUseCase())
+        /// 공지사항 내려받기
+        container.register(type: FetchAnnouncementUseCase.self, DefaultFetchAnnouncementUseCase())
+        /// 공지사항 자세히 보기
+        container.register(type: FetchDetailAnnouncementUseCase.self, DefaultFetchDetailAnnouncementUseCase())
+        /// 유저 정보 내려받기
+        container.register(type: UserInfoUseCase.self, DefaultUserInfoUseCase())
+        /// 유저가 작성한 강의평가 내려받기
+        container.register(type: FetchUserEvaluationPostsUseCase.self, DefaultFetchUserEvaluationPostUseCase())
+        /// 유저가 작성한 시험정보 내려받기
+        container.register(type: FetchUserExamPostsUseCase.self, DefaultFetchUserExamPostsUseCase())
+        /// 강의평가 수정
+        container.register(type: UpdateEvaluationPostUseCase.self, DefaultUpdateEvaluationPostUseCase())
+        /// 시험정보 수정
+        container.register(type: UpdateExamPostUseCase.self, DefaultUpdateExamPostUseCase())
     }
 
     func registerViewModels() {
