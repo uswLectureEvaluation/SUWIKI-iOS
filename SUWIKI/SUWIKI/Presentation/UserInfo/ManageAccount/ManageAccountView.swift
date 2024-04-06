9a7fe6a8747c01c15af37d10e81c83ce26ee18c9
sozohoy <sozohoy@gmail.com>
2024-04-06 16:26:57 +0900

    feat: 계정관리 화면 구현

--- SUWIKI/SUWIKI/Presentation/UserInfo/ManageAccount/ManageAccountView.swift
+++ SUWIKI/SUWIKI/Presentation/UserInfo/ManageAccount/ManageAccountView.swift
@@ -8,8 +8,125 @@
 import SwiftUI
 
 struct ManageAccountView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(uiColor: .systemGray6)
+                .ignoresSafeArea()
+            VStack {
+                loginIdView
+                    .padding(.top, 20)
+                emailView
+                changePasswordButton
+                    .padding(.top, 8)
+                logoutButton
+                withdrawAccountButton
+                Spacer()
+            }
+            .navigationTitle("내 계정")
+        }
+    }
+
+    var title: some View {
+        Text("내 계정")
+            .font(.b2)
+
+    }
+
+    var loginIdView: some View {
+        VStack {
+            HStack {
+                Text("로그인 아이디")
+                    .font(.c1)
+                    .foregroundStyle(Color(uiColor: .gray95))
+                Spacer()
+                Text("iostest")
+                    .font(.b5)
+            }
+            .padding(.horizontal, 24)
+            .padding(.bottom, 6)
+            Divider()
+        }
+        .padding(.top, 6)
+    }
+
+    var emailView: some View {
+        VStack {
+            HStack {
+                Text("학교 인증 메일")
+                    .font(.c1)
+                    .foregroundStyle(Color(uiColor: .gray95))
+                Spacer()
+                Text("sozohoy@suwon.ac.kr")
+                    .font(.b5)
+                    .tint(.black)
+            }
+            .padding(.horizontal, 24)
+            .padding(.bottom, 6)
+            Divider()
+        }
+        .padding(.top, 6)
+    }
+
+    var changePasswordButton: some View {
+        NavigationLink {
+            Text("hi")
+        } label: {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.white)
+                .frame(height: 50)
+                .overlay {
+                    HStack {
+                        Text("비밀번호 변경")
+                            .font(.b2)
+                            .foregroundStyle(.black)
+                        Spacer()
+                    }
+                    .padding(.leading, 16)
+                }
+        }
+        .padding(.horizontal, 24)
+
+    }
+
+    var logoutButton: some View {
+        Button {
+
+        } label: {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.white)
+                .frame(height: 50)
+                .overlay {
+                    HStack {
+                        Text("로그아웃")
+                            .font(.b2)
+                            .foregroundStyle(.black)
+                        Spacer()
+                    }
+                    .padding(.leading, 16)
+                }
+        }
+        .padding(.horizontal, 24)
+    }
+
+    var withdrawAccountButton: some View {
+        NavigationLink {
+
+        } label: {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.white)
+                .frame(height: 50)
+                .overlay {
+                    HStack {
+                        Text("회원탈퇴")
+                            .font(.b2)
+                            .foregroundStyle(.black)
+                        Spacer()
+                    }
+                    .padding(.leading, 16)
+                }
+        }
+        .padding(.horizontal, 24)
     }
 }
 
