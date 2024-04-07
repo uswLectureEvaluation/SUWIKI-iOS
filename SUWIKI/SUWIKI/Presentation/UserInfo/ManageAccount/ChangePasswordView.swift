d0f40844d4110dbc0c24a7fdcafaba1301d5ad30
sozohoy <sozohoy@gmail.com>
2024-04-07 22:08:19 +0900

    add: 비밀번호 변경 화면

---
+++ SUWIKI/SUWIKI/Presentation/UserInfo/ManageAccount/ChangePasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 4/6/24.
+//
+
+import SwiftUI
+
+struct ChangePasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePasswordView()
+}
