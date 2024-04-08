8558b03327bb6ccb6adb5d162cbe9e51071260da
sozohoy <sozohoy@gmail.com>
2024-04-08 20:42:19 +0900

    feat: refresh retry 되지 않도록 수정, 뮤텍스 적용

--- SUWIKI/SUWIKI/Core/Network/Interceptor.swift
+++ SUWIKI/SUWIKI/Core/Network/Interceptor.swift
@@ -12,7 +12,7 @@ import Alamofire
 final class BaseInterceptor: RequestInterceptor {
 
     let keychainManager = KeychainManager.shared
-
+    private let lock = NSLock()
 
     /// 변수에 urlRequest를 복사하지 말고 refresh 토큰일경우 그대로 한번 보내보자
     func adapt(
@@ -20,14 +20,16 @@ final class BaseInterceptor: RequestInterceptor {
         for session: Session,
         completion: @escaping (Result<URLRequest, Error>) -> Void
     ) {
+        print(#function)
         var request = urlRequest
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         if request.url?.absoluteString.contains("/refresh") == false {
-          if let accessToken = keychainManager.read(token: .AccessToken) {
-             request.setValue(accessToken, forHTTPHeaderField: "Authorization")
-         }
+            if let accessToken = keychainManager.read(token: .AccessToken) {
+                request.setValue(accessToken, forHTTPHeaderField: "Authorization")
+            }
         } else {
             if let refreshToken = keychainManager.read(token: .RefreshToken) {
+                print("Hi")
                 request.setValue(refreshToken, forHTTPHeaderField: "Authorization")
             }
         }
@@ -40,12 +42,15 @@ final class BaseInterceptor: RequestInterceptor {
         dueTo error: Error,
         completion: @escaping (RetryResult) -> Void
     ) {
+        lock.lock()
+        defer { lock.unlock() }
         guard let refreshToken = keychainManager.read(token: .RefreshToken),
-                request.response?.statusCode == 401 else {
+              request.response?.statusCode == 401,
+              let urlString = request.response?.url?.absoluteString,
+              !urlString.contains("refresh") else {
             completion(.doNotRetryWithError(error))
             return
         }
-
         let target = APITarget.User.refresh(DTO.RefreshRequest(authorization: refreshToken))
         APIProvider.requestRefreshToken(DTO.RefreshResponse.self, target: target) { [weak self] response in
             switch response.result {
@@ -59,6 +64,7 @@ final class BaseInterceptor: RequestInterceptor {
                 self.keychainManager.create(token: .RefreshToken, value: tokens.RefreshToken)
                 completion(.retry)
             case .failure:
+                print("@Log retry error")
                 completion(.doNotRetryWithError(error))
             }
         }
