e2f3e192fd6aa40ee3fbb24013cf4a31eed10ea2
sozohoy <sozohoy@gmail.com>
2024-04-09 20:38:47 +0900

    add: Files

---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/FindIDView.swift
@@ -0,0 +1,18 @@
+//
+//  FindIDView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 4/9/24.
+//
+
+import SwiftUI
+
+struct FindIDView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FindIDView()
+}
---
+++ SUWIKI/SUWIKI/Presentation/LectureEvaluation/Login/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 4/9/24.
+//
+
+import Foundation
