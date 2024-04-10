a0f988ec91fce7222d78598e316035905dab8877
sozohoy <sozohoy@gmail.com>
2024-04-10 20:00:30 +0900

    feat: UserPoint ViewModel 구현

--- SUWIKI/SUWIKI/Presentation/UserInfo/Info/UserPointViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/UserInfo/Info/UserPointViewModel.swift
@@ -8,5 +8,30 @@
 import Foundation
 
 final class UserPointViewModel: ObservableObject {
-    
+
+    @Inject var useCase: FetchPurchasedExamPostsUseCase
+
+    let point: Int
+    let writtenEvaluationPosts: Int
+    let writtenExamPosts: Int
+    let purchasedExamPosts: Int
+    @Published var purchasedPosts: [PurchasedPost] = []
+
+    init(
+        point: Int,
+        writtenEvaluationPosts: Int,
+        writtenExamPosts: Int,
+        purchasedExamPosts: Int
+    ) {
+        self.point = point
+        self.writtenEvaluationPosts = writtenEvaluationPosts
+        self.writtenExamPosts = writtenExamPosts
+        self.purchasedExamPosts = purchasedExamPosts
+        Task {
+            let posts = try await useCase.execute()
+            await MainActor.run {
+                self.purchasedPosts = posts
+            }
+        }
+    }
 }
