984ceaac3bc46be3add381cacdcff4e1569bf0a1
sozohoy <sozohoy@gmail.com>
2024-04-11 18:30:48 +0900

    feat: 회원가입 기능 구현

--- SUWIKI/SUWIKI/Presentation/UserInfo/ManageAccount/WithDrawAccountView.swift
+++ SUWIKI/SUWIKI/Presentation/UserInfo/ManageAccount/WithDrawAccountView.swift
@@ -8,8 +8,131 @@
 import SwiftUI
 
 struct WithDrawAccountView: View {
+
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var appState: AppState
+    @StateObject var viewModel = WithDrawAccountViewModel()
+    @FocusState var focusField: LoginViewInputType?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(uiColor: .systemGray6)
+                .ignoresSafeArea()
+            VStack {
+                inputViews(.id)
+                inputViews(.password)
+                Spacer()
+                withDrawButton
+            }
+        }
+        .navigationTitle("회원 탈퇴")
+        .navigationBarTitleDisplayMode(.large)
+        .alert("회원탈퇴 하시겠어요?", isPresented: $viewModel.isAlertPresented) {
+            Button("확인") {
+                Task {
+                    try await viewModel.withDraw()
+                }
+            }
+            Button(role: .cancel) {
+            } label: {
+                Text("취소")
+            }
+        }
+        .alert("계정 정보가 일치하지 않습니다.", isPresented: $viewModel.isWithDrawFailure) {
+            Button("확인") { }
+        } message: {
+            Text("입력 정보를 확인해주세요.")
+        }
+        .alert("회원탈퇴 되었습니다.", isPresented: $viewModel.isWithDrawSuccess) {
+            Button("확인") { 
+                viewModel.deleteTokens()
+                appState.isLoggedIn = false
+                dismiss()
+            }
+        } message: {
+            Text("수위키를 이용해주셔서 감사합니다.")
+        }
+    }
+
+    private func inputViews(
+        _ type: LoginViewInputType
+    ) -> some View {
+        VStack {
+            HStack {
+                Text(type == .id ? "아이디" : "비밀번호")
+                    .font(.c2)
+                    .foregroundStyle(focusField == type ?
+                                     Color(uiColor: .primaryColor) : Color(uiColor: .gray95))
+                Spacer()
+            }
+            if type == .id {
+                HStack {
+                    TextField("아이디를 입력하세요.", text: $viewModel.id)
+                        .focused($focusField, equals: .id)
+                    if focusField == type {
+                        Button {
+                            viewModel.id = ""
+                        } label: {
+                            Image(systemName: "x.circle.fill")
+                                .foregroundStyle(Color(uiColor: .gray95))
+                                .frame(width: 18, height: 18)
+                        }
+                    }
+                }
+            } else {
+                HStack {
+                    if viewModel.isPasswordVisible {
+                        TextField("비밀번호를 입력하세요.", text: $viewModel.password)
+                            .focused($focusField, equals: .password)
+                    } else {
+                        SecureField("비밀번호를 입력하세요",text: $viewModel.password)
+                            .focused($focusField, equals: .password)
+                    }
+                    if focusField == type {
+                        Button {
+                            viewModel.isPasswordVisible.toggle()
+                        } label: {
+                            Image(systemName: viewModel.isPasswordVisible ? "lock" : "lock.slash")
+                                .foregroundStyle(Color(uiColor: .gray95))
+                                .frame(width: 18, height: 18)
+                        }
+                    }
+                    if focusField == type {
+                        Button {
+                            viewModel.password = ""
+                        } label: {
+                            Image(systemName: "x.circle.fill")
+                                .foregroundStyle(Color(uiColor: .gray95))
+                                .frame(width: 18, height: 18)
+                        }
+                    }
+                }
+            }
+            Rectangle()
+                .frame(height: 1)
+                .foregroundStyle(focusField == type ?
+                                 Color(uiColor: .primaryColor) : Color(uiColor: .gray95))
+                .padding(.top, 4)
+        }
+        .padding(.top, type == .id ? 26 : 20)
+        .padding(.horizontal, 24)
+    }
+
+    private var withDrawButton: some View {
+        Button {
+            viewModel.isAlertPresented.toggle()
+        } label: {
+            RoundedRectangle(cornerRadius: 15.0)
+                .frame(height: 50)
+                .overlay {
+                    Text("회원탈퇴")
+                        .font(.h5)
+                        .foregroundStyle(Color(uiColor: .white))
+                }
+        }
+        .disabled(!viewModel.isButtonEnabled)
+        .padding(.horizontal, 24)
+        .padding(.bottom, 24)
     }
 }
 
--- SUWIKI/SUWIKI/Presentation/UserInfo/ManageAccount/WithDrawAccountViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/UserInfo/ManageAccount/WithDrawAccountViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import Combine
+
+final class WithDrawAccountViewModel: ObservableObject {
+
+    @Inject var useCase: WithDrawUseCase
+
+    @Published var id: String = ""
+    @Published var password: String = ""
+    @Published var isPasswordVisible = false
+    @Published var isButtonEnabled = false
+    @Published var isAlertPresented = false
+    @Published var isWithDrawSuccess = false
+    @Published var isWithDrawFailure = false
+
+    var cancellables = Set<AnyCancellable>()
+
+    init() {
+        $id
+            .combineLatest($password)
+            .receive(on: RunLoop.main)
+            .map {
+                print($0.count)
+                print($1.count)
+                return $0.count >= 6 && $0.count < 20 && $1.count >= 8 && $1.count < 20
+            }
+            .sink { [weak self] bool in
+                print(bool)
+                guard let self = self else { return }
+                self.isButtonEnabled = bool
+            }
+            .store(in: &cancellables)
+    }
+
+    func withDraw() async throws {
+        do {
+            let value = try await useCase.execute(id: self.id, password: self.password)
+            await MainActor.run {
+                if value {
+                    self.isWithDrawSuccess.toggle()
+                } else {
+                    self.isWithDrawFailure.toggle()
+                }
+            }
+        } catch {
+            await MainActor.run {
+                self.isWithDrawFailure.toggle()
+            }
+        }
+    }
+
+    func deleteTokens() {
+        KeychainManager.shared.delete(token: .AccessToken)
+        KeychainManager.shared.delete(token: .RefreshToken)
+    }
+
+}
