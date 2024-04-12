69a4c0cd65bbada16a41c4d76906ab49b20f0089
sozohoy <sozohoy@gmail.com>
2024-04-12 16:07:27 +0900

    feat: 탭바 이미지 추가

--- SUWIKI/SUWIKI/Presentation/TabBar/TabBarView.swift
+++ SUWIKI/SUWIKI/Presentation/TabBar/TabBarView.swift
@@ -11,28 +11,33 @@ struct TabBarView: View {
     
     let lectureEvaluationHomeView = DIContainer.shared.resolve(type: LectureEvaluationHomeView.self)
     let appState: AppState
-    
+
+    init(appState: AppState) {
+        self.appState = appState
+        UITabBar.appearance().standardAppearance.backgroundColor = .white
+    }
+
     var body: some View {
         TabView {
             TimetableWrapperView()
                 .ignoresSafeArea()
                 .tabItem {
+                    Image(systemName: "calendar.badge.clock")
                     Text("시간표")
                 }
             lectureEvaluationHomeView
                 .environmentObject(appState)
                 .tabItem {
+                    Image(systemName: "books.vertical")
                     Text("강의평가")
                 }
             UserInfoView()
                 .environmentObject(appState)
                 .tabItem {
+                    Image(systemName: "person")
                     Text("내 정보")
                 }
         }
+
     }
 }
-
-//#Preview {
-//    TabBarView()
-//}
