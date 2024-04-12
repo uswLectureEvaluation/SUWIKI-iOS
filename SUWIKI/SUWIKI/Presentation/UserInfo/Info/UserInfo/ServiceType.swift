dcaf1b168b3bcb62f3ca51aad04243e94478a390
sozohoy <sozohoy@gmail.com>
2024-04-12 16:07:37 +0900

    feat: 웹 URL 추가

--- SUWIKI/SUWIKI/Presentation/UserInfo/Info/UserInfo/ServiceType.swift
+++ SUWIKI/SUWIKI/Presentation/UserInfo/Info/UserInfo/ServiceType.swift
@@ -22,4 +22,15 @@ enum ServiceType: CaseIterable {
             "개인정보 처리 방침"
         }
     }
+
+    var url: URL {
+        switch self {
+        case .feedback:
+            URL(string: "https://docs.google.com/forms/d/e/1FAIpQLSepxjMAp4KKzQGZ0MFOWnI6CjfXSfi20VPGffXMfxzcMQyBhQ/viewform")!
+        case .termsOfService:
+            URL(string: "https://sites.google.com/view/suwiki-policy-terms")!
+        case .privacyPolicy:
+            URL(string: "https://sites.google.com/view/suwiki-policy-privacy")!
+        }
+    }
 }
