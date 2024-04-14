52586f41f5634ba06bff34c3b43b423b9acf4fee
sozohoy <sozohoy@gmail.com>
2024-04-14 18:42:39 +0900

    feat: 강의 삭제 유즈케이스

--- SUWIKI/SUWIKI/Domain/UseCases/Timetable/DeleteCourseUseCase.swift
+++ SUWIKI/SUWIKI/Domain/UseCases/Timetable/DeleteCourseUseCase.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+protocol DeleteCourseUseCase {
+    func execute(id: String, courseId: String)
+}
+
+final class DefaultDeleteCourseUseCase: DeleteCourseUseCase {
+    @Inject var repository: TimetableRepository
+
+    func execute(id: String, courseId: String) {
+        repository.deleteCourse(id: id, courseId: courseId)
+    }
+}
