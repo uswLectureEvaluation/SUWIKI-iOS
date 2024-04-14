adcc5a4c60d6bf438317480962b155ab48f0a54d
sozohoy <sozohoy@gmail.com>
2024-04-14 18:42:04 +0900

    feat: 레포지토리 인터페이스 선언

--- SUWIKI/SUWIKI/Domain/Repositories/TimetableRepository.swift
+++ SUWIKI/SUWIKI/Domain/Repositories/TimetableRepository.swift
@@ -9,6 +9,15 @@ import Foundation
 
 protocol TimetableRepository {
     func saveTimetable(name: String, semester: String)
+    func saveCourse(id: String, course: TimetableCourse)
+    func updateTimetableTitle(id: String, title: String)
     func fetchTimetable(id: String) -> Timetable?
     func fetchCourses(id: String) -> [Course]?
+    func fetchELearning(id: String) -> [Course]
+    func fetchFirebaseCourse(major: String) -> [FirebaseCourse]
+    func fetchMajors() -> [String]
+    func fetchTimetableList() -> [Timetable]
+    func deleteCourse(id: String, courseId: String)
+    func deleteTimetable(id: String)
+    func checkCourseVersion() async throws
 }
