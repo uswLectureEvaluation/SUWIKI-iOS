6a28ae388a22271c13ba82126f06cd12a901b426
sozohoy <sozohoy@gmail.com>
2024-04-14 18:41:12 +0900

    feat: 레포지토리 구현

--- SUWIKI/SUWIKI/Data/Repositories/DefaultTimetableRepository.swift
+++ SUWIKI/SUWIKI/Data/Repositories/DefaultTimetableRepository.swift
@@ -9,14 +9,31 @@ import Foundation
 
 final class DefaultTimetableRepository: TimetableRepository {
 
-    @Inject var storage: TimetableStorage
+    @Inject var coreDataStorage: CoreDataStorage
+    @Inject var firebaseStorage: FirebaseStorage
 
     func saveTimetable(
         name: String,
         semester: String
     ) {
         do {
-            try storage.saveTimetable(name: name, semester: semester)
+            try coreDataStorage.saveTimetable(name: name, semester: semester)
+        } catch {
+            print("@Log - \(error.localizedDescription)")
+        }
+    }
+
+    func saveCourse(id: String, course: TimetableCourse) {
+        do {
+            try coreDataStorage.saveCourse(id: id, course: course)
+        } catch {
+            print("@Log - \(error.localizedDescription)")
+        }
+    }
+
+    func updateTimetableTitle(id: String, title: String) {
+        do {
+            try coreDataStorage.updateTimetableTitle(id: id, title: title)
         } catch {
             print("@Log - \(error.localizedDescription)")
         }
@@ -24,7 +41,7 @@ final class DefaultTimetableRepository: TimetableRepository {
 
     func fetchTimetable(id: String) -> Timetable? {
         do {
-            return try storage.fetchTimetable(id: id)
+            return try coreDataStorage.fetchTimetable(id: id)
         } catch {
             return nil
         }
@@ -32,9 +49,70 @@ final class DefaultTimetableRepository: TimetableRepository {
 
     func fetchCourses(id: String) -> [Course]? {
         do {
-            return try storage.fetchCourses(id: id)
+            return try coreDataStorage.fetchCourses(id: id)
         } catch {
             return nil
         }
     }
+
+    func fetchFirebaseCourse(major: String) -> [FirebaseCourse] {
+        do {
+            return try coreDataStorage.fetchFirebaseCourse(major: major)
+        } catch {
+            return []
+        }
+    }
+
+    func fetchMajors() -> [String] {
+        do {
+            return try coreDataStorage.fetchMajors()
+        } catch {
+            return []
+        }
+    }
+
+    func fetchTimetableList() -> [Timetable] {
+        do {
+            return try coreDataStorage.fetchTimetableList()
+        } catch {
+            print("@Log - \(error.localizedDescription)")
+            return []
+        }
+    }
+
+    func fetchELearning(id: String) -> [Course] {
+        do {
+            return try coreDataStorage.fetchELearning(id: id)
+        } catch {
+            return []
+        }
+    }
+
+    func deleteCourse(
+        id: String,
+        courseId: String
+    ) {
+        do {
+            try coreDataStorage.deleteCourse(id: id, courseId: courseId)
+        } catch {
+            print("@Log - \(error.localizedDescription)")
+        }
+    }
+
+    func deleteTimetable(id: String) {
+        do {
+            try coreDataStorage.deleteTimetable(id: id)
+        } catch {
+            print("@Log - \(error.localizedDescription)")
+        }
+    }
+
+    func checkCourseVersion() async throws {
+        do {
+            try await firebaseStorage.isVersionChanged()
+        } catch {
+            print("@Log - \(error.localizedDescription)")
+        }
+    }
+
 }
