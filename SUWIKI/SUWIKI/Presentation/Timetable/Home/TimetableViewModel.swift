b8e511bc64c14628f440e0a00b3e04c9c3a9e47b
sozohoy <sozohoy@gmail.com>
2024-04-14 18:47:10 +0900

    feat: 코어데이터, 파이어베이스 의존성 제거 및 유즈케이스 연동

--- SUWIKI/SUWIKI/Presentation/Timetable/Home/TimetableViewModel.swift
+++ SUWIKI/SUWIKI/Presentation/Timetable/Home/TimetableViewModel.swift
@@ -7,98 +7,84 @@
 
 import Foundation
 
-import Combine
 import Elliotable
-import FirebaseRemoteConfig
 
 final class TimetableViewModel {
 
-    let coreDataManager = CoreDataManager.shared
-
-    private var cancellables: Set<AnyCancellable> = []
+    @Inject var fetchTimetableUseCase: FetchTimetableUseCase
+    @Inject var fetchCoursesUseCase: FetchCoursesUseCase
+    @Inject var saveTimeTableUseCase: SaveTimetableUseCase
+    @Inject var updateTimetableTitleUseCase: UpdateTimetableTitleUseCase
+    @Inject var deleteCourseUseCase: DeleteCourseUseCase
+    @Inject var fetchTimetableListUseCase: FetchTimetableListUseCase
+    @Inject var checkCourseVersionUseCase: CheckCourseVersionUseCase
 
     @Published var elliottEvent: [ElliottEvent] = []
     @Published var timetableTitle: String = ""
     @Published var timetableIsEmpty: Bool = false
-    @Published var versionChecked: Bool = false
+    @Published var isVersionChecked: Bool = false
 
     init() {
         fetchTimetable()
-        fetchRemoteConfig()
+        Task {
+            try await fetchFirebaseCourse()
+        }
     }
 
     func fetchTimetable() {
-        Task {
-            let timetable = await coreDataManager.fetchTimetableList()
-            updateCourse()
-            self.timetableIsEmpty = timetable.isEmpty
-        }
+        let timetable = fetchTimetableListUseCase.execute()
+        updateCourse()
+        self.timetableIsEmpty = timetable.isEmpty
     }
 
     func addTimetable() {
-        Task {
-            let calculateSemester = calculateSemester()
-            await coreDataManager.addTimeTable(name: calculateSemester.0,
-                                               semester: calculateSemester.1)
-            updateTimetable()
-            updateCourse()
-        }
+        let calculateSemester = calculateSemester()
+        saveTimeTableUseCase.execute(name: calculateSemester.0,
+                                     semester: calculateSemester.1)
+        updateTimetable()
+        updateCourse()
     }
 
     func updateTimetable() {
-        Task {
-            guard let id = UserDefaults.shared.value(forKey: "id") as? String,
-                  let title = await coreDataManager.fetchTimetable(id: id)?.name else {
-                self.timetableTitle = "시간표"
-                self.timetableIsEmpty = true
-                return
-            }
-            self.timetableTitle = title
-        }
+        guard let id = UserDefaults.shared.value(forKey: "id") as? String,
+              let title = fetchTimetableUseCase.execute(id: id)?.name else {
+                  self.timetableTitle = "시간표"
+                  self.timetableIsEmpty = true
+                  return
+              }
+        self.timetableTitle = title
     }
 
     func updateCourse() {
-        Task {
-            guard let id = UserDefaults.shared.value(forKey: "id") as? String else {
-                self.timetableIsEmpty = true
-                return
-            }
-            let course = await coreDataManager.fetchCourse(id: id) // userdefault.get
-            elliottEvent = []
-            for i in 0..<course.count {
-                let event = ElliottEvent(courseId: course[i].courseId ?? "",
-                                         courseName: course[i].courseName ?? "",
-                                         roomName: course[i].roomName ?? "",
-                                         professor: course[i].professor ?? "",
-                                         courseDay: ElliotDay(rawValue: Int(course[i].courseDay)) ?? .monday,
-                                         startTime: course[i].startTime ?? "",
-                                         endTime: course[i].endTime ?? "",
-                                         backgroundColor: .timetableColors[Int(course[i].timetableColor)])
-                elliottEvent.append(event)
-            }
-            self.timetableIsEmpty = false
+        guard let id = UserDefaults.shared.value(forKey: "id") as? String,
+              let course = fetchCoursesUseCase.execute(id: id)
+        else {
+            self.timetableIsEmpty = true
+            return
         }
+        elliottEvent = course.map { ElliottEvent(courseId: $0.courseId ?? "",
+                                                 courseName: $0.courseName ?? "",
+                                                 roomName: $0.roomName ?? "",
+                                                 professor: $0.professor ?? "",
+                                                 courseDay: ElliotDay(rawValue: Int($0.courseDay)) ?? .monday,
+                                                 startTime: $0.startTime ?? "",
+                                                 endTime: $0.endTime ?? "",
+                                                 backgroundColor: .timetableColors[Int($0.timetableColor)]) }
+        self.timetableIsEmpty = false
     }
 
     func updateTimetableTitle(title: String) {
-        Task {
-            guard let id = UserDefaults.shared.value(forKey: "id") as? String else { return }
-            try await coreDataManager.saveTitle(id: id, title: title)
-        }
+        guard let id = UserDefaults.shared.value(forKey: "id") as? String else { return }
+        updateTimetableTitleUseCase.execute(id: id, title: title)
+        self.timetableTitle = title
     }
 
-    func deleteCourse(uuid: String) {
-        Task {
-            guard let index = elliottEvent.firstIndex(where: { $0.courseId == uuid }),
-                  let id = UserDefaults.shared.value(forKey: "id") as? String
-            else { return }
-            elliottEvent.remove(at: index)
-            do {
-                try await coreDataManager.deleteCourse(id: id, courseId: uuid)
-            } catch {
-                await coreDataManager.handleCoreDataError(error)
-            }
-        }
+    func deleteCourse(courseId: String) {
+        guard let index = elliottEvent.firstIndex(where: { $0.courseId == courseId }),
+              let id = UserDefaults.shared.value(forKey: "id") as? String
+        else { return }
+        elliottEvent.remove(at: index)
+        deleteCourseUseCase.execute(id: id, courseId: courseId)
     }
 
     func calculateSemester() -> (String, String) {
@@ -111,28 +97,11 @@ final class TimetableViewModel {
         }
     }
 
-    func fetchRemoteConfig() {
-        let remoteConfig = RemoteConfig.remoteConfig()
-        let settings = RemoteConfigSettings()
-        settings.minimumFetchInterval = 0
-        remoteConfig.configSettings = settings
-        remoteConfig.fetch { (status, error) -> Void in
-            if status == .success {
-                remoteConfig.activate { changed, error in
-                    if changed {
-                        Task {
-                            await FirebaseManager.shared.fetchFirebaseCourse()
-                            self.versionChecked = true
-                            print("@Log - 시간표 최초 업데이트 ~")
-                        }
-                    } else {
-                        self.versionChecked = true
-                        print("@Log - 기존 시간표로 출력 ~")
-                    }
-                }
-            } else {
-                print(error?.localizedDescription ?? "")
-            }
+    func fetchFirebaseCourse() async throws {
+        try await checkCourseVersionUseCase.execute()
+        await MainActor.run {
+            self.isVersionChecked.toggle()
         }
     }
+
 }
