f0aa415e7bb4e4de8c272bbc4099a1f70b9ea53d
sozohoy <sozohoy@gmail.com>
2024-04-14 18:41:01 +0900

    feat: 구현체 구현

--- SUWIKI/SUWIKI/Data/DataSources/DefaultFirebaseStorage.swift
+++ SUWIKI/SUWIKI/Data/DataSources/DefaultFirebaseStorage.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Firebase
+import FirebaseDatabase
+
+final class DefaultFirebaseStorage: FirebaseStorage {
+
+    @Inject var coreDataStorage: CoreDataStorage
+    let firebaseManager = FirebaseManager.shared
+
+    func fetchCourse() async throws {
+        do {
+            let data = try await firebaseManager.ref.getData()
+            let course = snapshotToDictionary(snapshot: data)
+            try coreDataStorage.saveFirebaseCourse(course: course)
+        } catch {
+            print("@Log - \(error.localizedDescription)")
+        }
+    }
+    
+    func isVersionChanged() async throws {
+        let remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        let fetch = try await remoteConfig.fetch()
+        if fetch == .success {
+            let activate = try await remoteConfig.activate()
+            if activate {
+                try await fetchCourse()
+            }
+        }
+    }
+    
+
+}
