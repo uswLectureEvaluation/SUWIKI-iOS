c231c29f85cb07fb980dde854d17f44d89cfd0f2
sozohoy <sozohoy@gmail.com>
2024-04-21 19:56:47 +0900

    feat: 키체인 매니저

--- SUWIKI/Core/Keychain/KeychainManager.swift
+++ SUWIKI/Core/Keychain/KeychainManager.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+
+class KeychainManager {
+
+    static let shared = KeychainManager()
+    let service = "com.sozohoy.uswTimeTable"
+
+    private init() { }
+
+    func create(
+        token: TokenType,
+        value: String
+    ) {
+        /// Keychain Query
+        let keychainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: token.account,
+            kSecValueData: value.data(using: .utf8, allowLossyConversion: false)!
+        ]
+
+        /// Keychain의 Key값에 중복이 생길경우 저장할 수 없으므로 기존의 것을 삭제
+        SecItemDelete(keychainQuery)
+
+        /// 생성
+        let status: OSStatus = SecItemAdd(keychainQuery, nil)
+        assert(status == noErr, "@Log - Failed to save token")
+    }
+
+    func read(
+        token: TokenType
+    ) -> String? {
+
+        let keychainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: token.account,
+            /// CFData로 불러오는 것
+            kSecReturnData: true,
+            /// 중복되는 경우 하나의 값만 가져오게 하도록
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+
+        /// CFData -> AnyObject -> Data
+        var cfDataToAnyObject: AnyObject?
+        let status = SecItemCopyMatching(keychainQuery,
+                                         &cfDataToAnyObject)
+
+        if status == errSecSuccess {
+            let retrievedData = cfDataToAnyObject as! Data
+            let value = String(data: retrievedData, encoding: .utf8)
+            return value
+        } else {
+            print("@Log - Failed to read token - status code == \(status)")
+            return nil
+        }
+    }
+
+    func delete(
+        token: TokenType
+    ) {
+        let keychainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: token.account
+        ]
+
+        let status = SecItemDelete(keychainQuery)
+        assert(status == noErr, "@Log - Failed to delete token")
+    }
+
+}
