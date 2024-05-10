0e3354e131847d91c8cebfc5a9e343a126d42792
sozohoy <sozohoy@gmail.com>
2024-05-10 19:03:12 +0900

    refactor: 인터페이스 변경 사항 적용

--- SUWIKI/Data/Sources/Repositories/DefaultTimetableRepository.swift
+++ SUWIKI/Data/Sources/Repositories/DefaultTimetableRepository.swift
@@ -20,103 +20,123 @@ public final class DefaultTimetableRepository: TimetableRepository {
     public func saveTimetable(
         name: String,
         semester: String
-    ) {
+    ) -> Result<(), CoreDataError> {
         do {
             try coreDataStorage.saveTimetable(name: name, semester: semester)
+            return .success(())
         } catch {
             print("@Log - \(error.localizedDescription)")
+            return .failure(.saveError)
         }
     }
 
-    public func saveCourse(id: String, course: TimetableCourse) {
+    public func saveCourse(
+        id: String,
+        course: TimetableCourse
+    ) -> Result<(), CoreDataError> {
         do {
             try coreDataStorage.saveCourse(id: id, course: course)
+            return .success(())
         } catch {
             print("@Log - \(error.localizedDescription)")
+            return .failure(.saveError)
         }
     }
 
-    public func updateTimetableTitle(id: String, title: String) {
+    public func updateTimetableTitle(
+        id: String,
+        title: String
+    ) -> Result<(), CoreDataError> {
         do {
             try coreDataStorage.updateTimetableTitle(id: id, title: title)
+            return .success(())
         } catch {
             print("@Log - \(error.localizedDescription)")
+            return .failure(.saveError)
         }
     }
 
-    public func fetchTimetable(id: String) -> Domain.UserTimetable? {
+    public func fetchTimetable(id: String) -> Result<UserTimetable?, CoreDataError> {
         do {
-            return try coreDataStorage.fetchTimetable(id: id)
+            let userTimetable = try coreDataStorage.fetchTimetable(id: id)
+            return .success(userTimetable)
         } catch {
-            return nil
+            return .failure(.fetchError)
         }
     }
 
-    public func fetchCourses(id: String) -> [TimetableCourse]? {
+    public func fetchCourses(id: String) -> Result<[TimetableCourse]?, CoreDataError> {
         do {
-            return try coreDataStorage.fetchCourses(id: id)
+            let timetableCourse = try coreDataStorage.fetchCourses(id: id)
+            return .success(timetableCourse)
         } catch {
-            return nil
+            return .failure(.fetchError)
         }
     }
 
-    public func fetchFirebaseCourse(major: String) -> [FetchCourse] {
+    public func fetchFirebaseCourse(major: String) -> Result<[FetchCourse], CoreDataError> {
         do {
-            return try coreDataStorage.fetchFirebaseCourse(major: major)
+            let fetchCourse = try coreDataStorage.fetchFirebaseCourse(major: major)
+            return .success(fetchCourse)
         } catch {
-            return []
+            return .failure(.fetchError)
         }
     }
 
-    public func fetchMajors() -> [String] {
+    public func fetchMajors() -> Result<[String], CoreDataError> {
         do {
-            return try coreDataStorage.fetchMajors()
+            let fetchMajors = try coreDataStorage.fetchMajors()
+            return .success(fetchMajors)
         } catch {
-            return []
+            return .failure(.fetchError)
         }
     }
 
-    public func fetchTimetableList() -> [Domain.UserTimetable] {
+    public func fetchTimetableList() -> Result<[UserTimetable], CoreDataError> {
         do {
-            return try coreDataStorage.fetchTimetableList()
+            let userTimetable = try coreDataStorage.fetchTimetableList()
+            return .success(userTimetable)
         } catch {
-            print("@Log - \(error.localizedDescription)")
-            return []
+            return .failure(.fetchError)
         }
     }
 
-    public func fetchELearning(id: String) -> [TimetableCourse] {
+    public func fetchELearning(id: String) -> Result<[TimetableCourse], CoreDataError> {
         do {
-            return try coreDataStorage.fetchELearning(id: id)
+            let eLearning = try coreDataStorage.fetchELearning(id: id)
+            return .success(eLearning)
         } catch {
-            return []
+            return .failure(.fetchError)
         }
     }
 
     public func deleteCourse(
         id: String,
         courseId: String
-    ) {
+    ) -> Result<(), CoreDataError> {
         do {
             try coreDataStorage.deleteCourse(id: id, courseId: courseId)
+            return .success(())
         } catch {
-            print("@Log - \(error.localizedDescription)")
+            return .failure(.deleteError)
         }
     }
 
-    public func deleteTimetable(id: String) {
+    public func deleteTimetable(id: String) -> Result<(), CoreDataError> {
         do {
             try coreDataStorage.deleteTimetable(id: id)
+            return .success(())
         } catch {
-            print("@Log - \(error.localizedDescription)")
+            return .failure(.deleteError)
         }
     }
 
-    public func checkCourseVersion() async throws {
+    public func checkCourseVersion() async throws -> Result<(), CoreDataError> {
         do {
             try await firebaseStorage.isVersionChanged()
+            return .success(())
         } catch {
-            print("@Log - \(error.localizedDescription)")
+            return .failure(.contextError)
         }
     }
 
