6d11dc6c73ce7a5c40f13c56bac139f3dbe484b6
sozohoy <sozohoy@gmail.com>
2024-05-28 23:59:23 +0900

    test: MockAPIProvider 구현

--- SUWIKI/Data/DataTests/Mock/MockAPIProvider.swift
+++ SUWIKI/Data/DataTests/Mock/MockAPIProvider.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Network
+
+final class MockAPIProvider: APIProviderProtocol {
+
+    private var responses: [String: Any] = [:]
+    var isErrorOccured = false
+
+    func setResponse<T: Decodable>(
+        _ object: T.Type,
+        response: T
+    ) {
+        let key = String(describing: object)
+        responses[key] = response
+    }
+
+    func request<T: Decodable>(
+        _ object: T.Type,
+        target: TargetType
+    ) async throws -> T {
+        if isErrorOccured {
+            throw NSError(domain: "MockError", code: 0)
+        }
+        let key = String(describing: object)
+        guard let response = responses[key] as? T else {
+            fatalError("Unmocked request type: \(object)")
+        }
+
+        return response
+    }
+
+    func request(target: TargetType) async throws -> Bool {
+        return !isErrorOccured ? true : false
+    }
+}
