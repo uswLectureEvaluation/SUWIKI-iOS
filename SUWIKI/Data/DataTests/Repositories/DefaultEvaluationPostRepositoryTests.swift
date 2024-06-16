47c83801b7136cfee3c690f72eb1996c0f978cae
sozohoy <sozohoy@gmail.com>
2024-06-17 03:25:59 +0900

    test: fetch 메소드 테스트 완료

--- SUWIKI/Data/DataTests/Repositories/DefaultEvaluationPostRepositoryTests.swift
+++ SUWIKI/Data/DataTests/Repositories/DefaultEvaluationPostRepositoryTests.swift
@@ -9,19 +9,104 @@ import XCTest
 @testable import Data
 
 final class DefaultEvaluationPostRepositoryTests: XCTestCase {
-  
+
   var mockAPIProvider: MockAPIProvider!
   var repository: DefaultEvaluationPostRepository!
 
   override func setUp() {
     super.setUp()
     mockAPIProvider = MockAPIProvider()
-    repository = DefaultExamPostRepository(apiProvider: mockAPIProvider)
+    repository = DefaultEvaluationPostRepository(
+      apiProvider: mockAPIProvider
+    )
   }
   
   override func tearDown() {
     repository = nil
     mockAPIProvider = nil
     super.tearDown()
+  }
+  
+  func testFetchEvaluationPosts() async throws {
+    let mockEvaluationPost = DTO.EvaluationPostResponse(
+      id: 0,
+      selectedSemester: "2022-2",
+      totalAvg: 0,
+      satisfaction: 0,
+      learning: 0,
+      honey: 0,
+      team: 0,
+      difficulty: 0,
+      homework: 0,
+      content: "evaluationTest"
+    )
+    let mockResponse = DTO.FetchEvaluationPostsResponse(
+      posts: [mockEvaluationPost],
+      written: true
+    )
+    mockAPIProvider.setResponse(
+      DTO.FetchEvaluationPostsResponse.self,
+      response: mockResponse
+    )
+    
+    let evalaution = try await repository.fetch(
+      lectureId: 0,
+      page: 0
+    )
+    XCTAssertEqual(
+      evalaution.posts.count,
+      1
+    )
+    XCTAssertEqual(
+      evalaution.posts.first?.id,
+      mockEvaluationPost.id
+    )
+    XCTAssertEqual(
+      evalaution.written,
+      true
+    )
+  }
+  
+  func testFetchUserPosts() async throws {
+    let mockEvaluationPost = DTO.FetchUserEvaluationPostsResponse.UserEvaluationPostResponse(
+      id: 1,
+      lectureName: "",
+      professor: "",
+      majorType: "",
+      selectedSemester: "",
+      semesterList: "",
+      totalAvg: 0,
+      satisfaction: 0,
+      learning: 0,
+      honey: 0,
+      team: 0,
+      difficulty: 0,
+      homework: 0,
+      content: "evaluationTest"
+    )
+    let mockResponse = DTO.FetchUserEvaluationPostsResponse(
+      posts: [mockEvaluationPost],
+      statusCode: 200,
+      message: "success"
+    )
+    mockAPIProvider.setResponse(
+      DTO.FetchUserEvaluationPostsResponse.self,
+      response: mockResponse
+    )
+    
+    let userPosts = try await repository.fetchUserPosts()
+    
+    XCTAssertEqual(
+      userPosts.count,
+      1
+    )
+    XCTAssertEqual(
+      userPosts.first?.id,
+      1
+    )
+    XCTAssertEqual(
+      userPosts.first?.content,
+      "evaluationTest"
+    )
   }
 }
