cda8b4c2ccf1d051364f53c97af866caafe36bf2
sozohoy <sozohoy@gmail.com>
2024-06-20 18:47:02 +0900

    test: Response 존재하는 메소드들 대상 UserRepoTests 완료

--- SUWIKI/Data/DataTests/Repositories/DefaultUserRepositoryTests.swift
+++ SUWIKI/Data/DataTests/Repositories/DefaultUserRepositoryTests.swift
@@ -6,36 +6,117 @@
 //
 
 import XCTest
+@testable import Data
 
 final class DefaultUserRepositoryTests: XCTestCase {
+  var mockAPIProvider: MockAPIProvider!
+  var repository: DefaultUserRepository!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func setUp() {
+    super.setUp()
+    mockAPIProvider = MockAPIProvider()
+    repository = DefaultUserRepository(
+      apiProvider: mockAPIProvider
+    )
+  }
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+  override func tearDown() {
+    repository = nil
+    mockAPIProvider = nil
+    super.tearDown()
+  }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+  func testLogin() async throws {
+    let mockResponse = DTO.TokenResponse(
+      AccessToken: "",
+      RefreshToken: ""
+    )
+    mockAPIProvider.setResponse(
+      DTO.TokenResponse.self,
+      response: mockResponse
+    )
+    
+    let success = try await repository.login(
+      id: "",
+      password: ""
+    )
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    XCTAssertEqual(
+      success,
+      true
+    )
+  }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+  func testCheckDuplicatedId() async throws {
+    let mockResponse = DTO.CheckDuplicatedIdResponse(
+      overlap: true
+    )
+    mockAPIProvider.setResponse(
+      DTO.CheckDuplicatedIdResponse.self,
+      response: mockResponse
+    )
+    
+    let duplicated = try await repository.checkDuplicatedId(
+      id: ""
+    )
+    
+    XCTAssertEqual(
+      duplicated,
+      true
+    )
+  }
+  
+  func testCheckDuplicatedEmail() async throws {
+    let mockResponse = DTO.CheckDuplicatedEmailResponse(
+      overlap: true
+    )
+    mockAPIProvider.setResponse(
+      DTO.CheckDuplicatedEmailResponse.self,
+      response: mockResponse
+    )
+    
+    let duplicated = try await repository.checkDuplicatedEmail(
+      email: ""
+    )
+    
+    XCTAssertEqual(
+      duplicated,
+      true
+    )
+  }
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  func testGetUserInfo() async throws {
+    let id = "iostest"
+    let email = "sozohoy@gmail.com"
+    let point = 2086
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    let mockResponse = DTO.UserInfoResponse(
+      loginId: id,
+      email: email,
+      point: point,
+      writtenEvaluation: 0,
+      writtenExam: 0,
+      viewExam: 0
+    )
+    mockAPIProvider.setResponse(
+      DTO.UserInfoResponse.self,
+      response: mockResponse
+    )
+
+    let userInfo = try await repository.userInfo()
+
+    XCTAssertEqual(
+      userInfo.id,
+      id
+    )
+    XCTAssertEqual(
+      userInfo.email,
+      email
+    )
+    XCTAssertEqual(
+      userInfo.point,
+      point
+    )
+  }
+
 }
