69645a1b6abec1cc93c3c2d76f3cf166897c039d
sozohoy <sozohoy@gmail.com>
2024-08-26 22:50:23 +0900

    chore: Presentation 모듈 생성

---
+++ SUWIKI/Presentation/Presentation/Presentation.h
@@ -0,0 +1,18 @@
+//
+//  Presentation.h
+//  Presentation
+//
+//  Created by 한지석 on 8/23/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Presentation.
+FOUNDATION_EXPORT double PresentationVersionNumber;
+
+//! Project version string for Presentation.
+FOUNDATION_EXPORT const unsigned char PresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presentation/PublicHeader.h>
+
+
---
+++ SUWIKI/Presentation/Presentation/Sources/Presentation.swift
@@ -0,0 +1,8 @@
+//
+//  Presentation.swift
+//  Presentation
+//
+//  Created by 한지석 on 8/23/24.
+//
+
+import Foundation
