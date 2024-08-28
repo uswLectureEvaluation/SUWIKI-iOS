43ed1e5f195e980ee83f0edc40b5420b9a9222c0
sozohoy <sozohoy@gmail.com>
2024-08-28 18:22:42 +0900

    refactor: addCourseView 리펙토링, 접근제어자 변경 및 캡슐화

--- SUWIKI/Presentation/Presentation/Sources/Timetable/AddLecture/Add/AddCourseView.swift
+++ SUWIKI/Presentation/Presentation/Sources/Timetable/AddLecture/Add/AddCourseView.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
+import Domain
+
 import Then
 import SnapKit
 
 class AddCourseView: UIView {
   
-  //MARK: UI
-  
-  let classficationLabel = UILabel().then {
+  private let classficationLabel = UILabel().then {
     $0.layer.masksToBounds = true
     $0.layer.cornerRadius = 16
     $0.textAlignment = .center
@@ -22,95 +22,100 @@ class AddCourseView: UIView {
     $0.font = UIFont.systemFont(ofSize: 17, weight: .bold)
   }
   
-  let courseTitleLabel = UILabel().then {
+  private let courseTitleLabel = UILabel().then {
     $0.textColor = .gray
     $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
   }
   
-  let courseNameLabel = UILabel().then {
+  private let courseNameLabel = UILabel().then {
     $0.numberOfLines = 1
     $0.adjustsFontSizeToFitWidth = true
     $0.minimumScaleFactor = 0.5
     $0.font = UIFont.systemFont(ofSize: 34, weight: .bold)
   }
   
-  let gradeLabel = UILabel().then {
+  private let gradeLabel = UILabel().then {
     $0.textColor = .gray
     $0.font = UIFont.systemFont(ofSize: 20, weight: .regular)
   }
   
-  let barImage = UIImageView().then {
+  private let barImage = UIImageView().then {
     $0.tintColor = .gray
     $0.image = UIImage(named: "bar")
   }
   
-  let creditLabel = UILabel().then {
+  private let creditLabel = UILabel().then {
     $0.text = "3학점"
     $0.textColor = .gray
     $0.font = UIFont.systemFont(ofSize: 20, weight: .regular)
   }
   
-  let majorTitleLabel = UILabel().then {
+  private let majorTitleLabel = UILabel().then {
     $0.text = "학과"
     $0.textColor = .gray
     $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
   }
   
-  let majorLabel = UILabel().then {
+  private let majorLabel = UILabel().then {
     $0.text = "컴퓨터SW"
     $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
   }
   
-  let professorTitleLabel = UILabel().then {
+  private let professorTitleLabel = UILabel().then {
     $0.text = "교수"
     $0.textColor = .gray
     $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
   }
   
-  let professorLabel = UILabel().then {
+  private let professorLabel = UILabel().then {
     $0.text = "김대엽"
     $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
   }
   
-  let roomNameLabel = UILabel().then {
+  private let roomNameLabel = UILabel().then {
     $0.textColor = .white
     $0.text = "IT301(목3,4)"
     $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
   }
   
-  let bottomBackground = UIView().then {
+  private let bottomBackground = UIView().then {
     $0.layer.maskedCorners = [CACornerMask.layerMinXMaxYCorner, CACornerMask.layerMaxXMaxYCorner]
     $0.layer.cornerRadius = 12
   }
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    setupUI()
-    setupConstraints()
-    self.backgroundColor = .white
+    setStyle()
+    setUI()
+    setLayout()
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
-  private func setupUI() {
-    addSubview(bottomBackground)
-    addSubview(classficationLabel)
-    addSubview(courseTitleLabel)
-    addSubview(courseNameLabel)
-    addSubview(gradeLabel)
-    addSubview(barImage)
-    addSubview(creditLabel)
-    addSubview(majorTitleLabel)
-    addSubview(majorLabel)
-    addSubview(professorTitleLabel)
-    addSubview(professorLabel)
-    addSubview(roomNameLabel)
+
+  private func setStyle() {
+    backgroundColor = .white
+  }
+
+  private func setUI() {
+    [
+      bottomBackground,
+      classficationLabel,
+      courseTitleLabel,
+      courseNameLabel,
+      gradeLabel,
+      barImage,
+      creditLabel,
+      majorTitleLabel,
+      majorLabel,
+      professorTitleLabel,
+      professorLabel,
+      roomNameLabel
+    ].forEach { addSubview($0) }
   }
   
-  private func setupConstraints() {
-    
+  private func setLayout() {
     classficationLabel.snp.makeConstraints {
       $0.top.equalTo(self.snp.top).offset(12)
       $0.trailing.equalTo(self.snp.trailing).offset(-12)
@@ -185,32 +190,20 @@ class AddCourseView: UIView {
       $0.centerY.equalTo(bottomBackground.snp.centerY)
       $0.height.equalTo(22)
     }
-    
   }
   
-  func updateUI(searchText: String) { }
-}
-
-class RectangleView: UIView {
-  override func draw(_ rect: CGRect) {
-    super.draw(rect)
-    // 사각형을 그릴 색상 설정
-    UIColor.white.setFill()
-    // 사각형을 그릴 경로 생성
-    let rectanglePath = UIBezierPath(rect: rect)
-    // 경로를 채우기
-    rectanglePath.fill()
-  }
-}
-
-extension UIView {
-  func roundBottomCorners(radius: CGFloat) {
-    let path = UIBezierPath(roundedRect: bounds,
-                            byRoundingCorners: [.bottomLeft, .bottomRight],
-                            cornerRadii: CGSize(width: radius, height: radius))
-    let maskLayer = CAShapeLayer()
-    maskLayer.path = path.cgPath
-    maskLayer.fillColor = UIColor.blue.cgColor
-    layer.mask = maskLayer
+  func updateUI(
+    course: FetchCourse,
+    _ colorNumber: Int
+  ) {
+    classficationLabel.text = course.classification
+    courseNameLabel.text = course.courseName
+    addCourseView.gradeLabel.text = "\(course.num)학년"
+    addCourseView.creditLabel.text = "\(course.credit)학점"
+    addCourseView.majorLabel.text = course.major
+    addCourseView.professorLabel.text = course.professor
+    addCourseView.roomNameLabel.text = course.roomName
+    addCourseView.bottomBackground.backgroundColor = .timetableColors[colorNumber]
+    addCourseView.classficationLabel.backgroundColor = .timetableColors[colorNumber]
   }
 }
