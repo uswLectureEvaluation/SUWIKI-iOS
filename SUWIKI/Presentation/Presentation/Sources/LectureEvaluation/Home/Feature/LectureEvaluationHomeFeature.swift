9b75c3d182c1465c7e2e78ad79ffc564b3d4b0fa
sozohoy <sozohoy@gmail.com>
2024-09-04 21:33:45 +0900

    add: LectureEvaluationHomeFeature

---
+++ SUWIKI/Presentation/Presentation/Sources/LectureEvaluation/Home/Feature/LectureEvaluationHomeFeature.swift
@@ -0,0 +1,8 @@
+//
+//  LectureEvaluationHomeFeature.swift
+//  Presentation
+//
+//  Created by 한지석 on 9/4/24.
+//
+
+import Foundation
--- SUWIKI/Presentation/Presentation/Sources/Presentation.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Presentation.swift
-//  Presentation
-//
-//  Created by 한지석 on 8/23/24.
-//
-
-import Foundation
