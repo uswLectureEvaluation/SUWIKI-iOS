1939fa730608842e6d5949ecaaa558a979a84a6f
sozohoy <sozohoy@gmail.com>
2024-09-09 16:19:50 +0900

    refactor: RootView 생성, LectureEvaluationHomeView에 Feature 의존성 주입

---
+++ SUWIKI/Presentation/Presentation/Sources/Root/RootView.swift
@@ -0,0 +1,65 @@
+//
+//  TabBarView.swift
+//  SUWIKI
+//
+//  Created by 한지석 on 4/2/24.
+//
+
+import SwiftUI
+
+import Common
+import Domain
+import DIContainer
+
+import ComposableArchitecture
+
+public struct RootView: View {
+  
+  let store: StoreOf<RootFeature>
+  
+  public init() {
+    self.store = Store(initialState: RootFeature.State()) {
+      RootFeature()
+    }
+  }
+  
+  public var body: some View {
+    TabBarView(store: store)
+  }
+}
+
+struct TabBarView: View {
+  
+  let store: StoreOf<RootFeature>
+  
+  init(store: StoreOf<RootFeature>) {
+    self.store = store
+    UITabBar.appearance().standardAppearance.backgroundColor = .white
+  }
+  
+  public var body: some View {
+    TabView {
+      TimetableWrapperView()
+        .ignoresSafeArea()
+        .tabItem {
+          Image(systemName: "calendar.badge.clock")
+          Text("시간표")
+        }
+      LectureEvaluationHomeView(
+        store: store.scope(
+          state: \.lectureEvaluation,
+          action: \.lectureEvaluation
+        )
+      )
+      .tabItem {
+        Image(systemName: "books.vertical")
+        Text("강의평가")
+      }
+      UserInfoView()
+        .tabItem {
+          Image(systemName: "person")
+          Text("내 정보")
+        }
+    }
+  }
+}
